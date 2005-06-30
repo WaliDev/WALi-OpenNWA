1587d7b631f145fa23348d48c569e7be8acb308e
Nick Kidd <kidd@cs.wisc.edu>
2005-06-30 02:28:13 +0000

    Added t4.cpp to test path_summary. Needs lots of work.
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@13 dfa36f5b-b2fa-0310-9605-e5aca7e64462

---
+++ Tests/t4.cpp
@@ -0,0 +1,71 @@
+/*!
+ * @author Nick Kidd
+ *
+ * Tests path_summary
+ */
+#include <iostream>
+#include <fstream>
+#include "wali/KeyFactory.hpp"
+#include "wali/wfa/WFA.hpp"
+#include "wali/wpds/WitnessWPDS.hpp"
+// For debug info in main()
+#include "wali/wfa/Trans.hpp"
+#include "wali/wfa/State.hpp"
+
+// Reachability Weight Domain
+#include "Examples/Reach/Reach.hpp"
+
+//
+// n0:
+//      if( ?? )
+// n1:      x
+//      else
+// n2:      y
+// n3:  ...
+//
+void dot()
+{
+    using wali::wpds::WitnessWPDS;
+    using wali::wpds::WPDS;
+    using wali::KeyFactory;
+    using wali::wali_key_t;
+    using wali::sem_elem_t;
+    using wali::wfa::WFA;
+    using std::cout;
+    Reach *reach = new Reach(true);
+
+    wali_key_t p = KeyFactory::get_key("p");
+    wali_key_t acc1 = KeyFactory::get_key("acc1");
+    wali_key_t acc2 = KeyFactory::get_key("acc2");
+    wali_key_t n0 = KeyFactory::get_key("n0");
+    wali_key_t n1 = KeyFactory::get_key("n1");
+    wali_key_t n2 = KeyFactory::get_key("n2");
+    wali_key_t n3 = KeyFactory::get_key("n3");
+
+    WFA fa;
+    fa.set_initial_state(p);
+    fa.add_final_state(acc1);
+    fa.add_final_state(acc2);
+    fa.add_trans( p, KeyFactory::get_key("n0"), acc1, reach);
+    fa.add_trans( p, n0, p, reach);
+    fa.add_trans( p, n1, KeyFactory::get_key("q"), reach);
+    fa.add_trans( KeyFactory::get_key("q"), n2, acc2, reach);
+    fa.add_trans( acc2, n3, acc2, reach);
+
+    fa.path_summary();
+}
+
+int main()
+{
+    dot();
+    std::cerr << "# Trans : " << wali::wfa::Trans::numTrans << std::endl;
+    std::cerr << "# States : " << wali::wfa::State::numStates << std::endl;
+    std::cerr << "# Reaches : " << Reach::numReaches << std::endl;
+    return 0;
+}
+
+/* Yo, Emacs!
+   ;;; Local Variables: ***
+   ;;; tab-width: 4 ***
+   ;;; End: ***
+*/
