8e71ce803b9f3e965dfadd97b4ab938736d47fa7
Nick Kidd <kidd@cs.wisc.edu>
2005-10-08 22:29:30 +0000

    removed wpds namespace from DefaultWorklist
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@72 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Source/wali/DefaultWorklist.cpp
+++ Source/wali/DefaultWorklist.cpp
@@ -8,54 +8,50 @@
 
 namespace wali
 {
-    namespace wpds
+    DefaultWorklist::DefaultWorklist() : Worklist() {}
+
+    DefaultWorklist::~DefaultWorklist() {
+        // clear all items that may still be in
+        // this worklist
+        clear();
+    }
+
+    void DefaultWorklist::put( Markable * item )
     {
-        DefaultWorklist::DefaultWorklist() : Worklist() {}
-
-        DefaultWorklist::~DefaultWorklist() {
-            // clear all items that may still be in
-            // this worklist
-            clear();
+        if( !item->marked() ) {
+            item->mark();
+            wl.push_back( item );
         }
+    }
 
-        void DefaultWorklist::put( Markable * item )
-        {
-            if( !item->marked() ) {
-                item->mark();
-                wl.push_back( item );
-            }
+    Markable * DefaultWorklist::get()
+    {
+        Markable * item = 0;
+        if( !empty() ) {
+            item = wl.back();
+            wl.pop_back();
+            item->unmark();
         }
+        return item;
+    }
 
-        Markable * DefaultWorklist::get()
-        {
-            Markable * item = 0;
-            if( !empty() ) {
-                item = wl.back();
-                wl.pop_back();
-                item->unmark();
-            }
-            return item;
+    bool DefaultWorklist::empty() const
+    {
+        return wl.empty();
+    }
+
+    void DefaultWorklist::clear()
+    {
+        std::list< Markable * >::iterator it = wl.begin();
+
+        // iterate through turning off inworklist flag
+        for( ; it != wl.end() ; it++ ) {
+            Markable * item = *it;
+            item->unmark();
         }
-
-        bool DefaultWorklist::empty() const
-        {
-            return wl.empty();
-        }
-
-        void DefaultWorklist::clear()
-        {
-            std::list< Markable * >::iterator it = wl.begin();
-
-            // iterate through turning off inworklist flag
-            for( ; it != wl.end() ; it++ ) {
-                Markable * item = *it;
-                item->unmark();
-            }
-            // clear the list
-            wl.clear();
-        }
-
-    } // namespace wpds
+        // clear the list
+        wl.clear();
+    }
 
 } // namespace wali
 
