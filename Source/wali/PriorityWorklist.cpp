278b952674b84080a03b4237d666bedf14d544d1
Nick Kidd <kidd@cs.wisc.edu>
2005-10-21 14:16:19 +0000

    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@76 dfa36f5b-b2fa-0310-9605-e5aca7e64462

---
+++ Source/wali/PriorityWorklist.cpp
@@ -0,0 +1,74 @@
+/*!
+ * @author Nick Kidd
+ */
+
+#include "wali/Common.hpp"
+#include "wali/PriorityWorklist.hpp"
+
+namespace wali
+{
+    ////////
+    // class StackSymLT
+    ////////
+    bool StackSymLT::operator()(
+            const wfa::Trans* a,
+            const wfa::Trans* b) const
+    {
+        if( (a->stack() != WALI_EPSILON) && (b->stack() != WALI_EPSILON)) {
+            return a->stack() < b->stack();
+        }
+        else {
+            return a->stack() && (b->stack() == WALI_EPSILON);
+        }
+    }
+
+    ////////
+    // class PriorityWorklist
+    ////////
+    PriorityWorklist::PriorityWorklist() : Worklist() {}
+
+    PriorityWorklist::~PriorityWorklist()
+    {
+        clear();
+    }
+
+    void PriorityWorklist::put( wfa::Trans *t )
+    {
+        if( !t->marked() ) {
+            t->mark();
+            workset.insert(t);
+        }
+    }
+
+    wfa::Trans * PriorityWorklist::get() 
+    {
+        pwl_t::iterator i = workset.begin();
+        wfa::Trans* t = (*i);
+        workset.erase(i);
+        t->unmark();
+        return t;
+    }
+
+    bool PriorityWorklist::empty() const
+    {
+        return workset.empty();
+    }
+
+    void PriorityWorklist::clear()
+    {
+        pwl_t::iterator i = workset.begin();
+        pwl_t::iterator iEND = workset.end();
+        // unmark everything
+        for( ; i != iEND ; i++ ) {
+            (*i)->unmark();
+        }
+        workset.clear();
+    }
+}
+
+/* Yo, Emacs!
+   ;;; Local Variables: ***
+   ;;; tab-width: 4 ***
+   ;;; End: ***
+*/
+
---
+++ Source/wali/PriorityWorklist.hpp
@@ -0,0 +1,58 @@
+#ifndef wali_PRIORITY_WORKLIST_GUARD
+#define wali_PRIORITY_WORKLIST_GUARD 1
+
+/*!
+ * @author Nick Kidd
+ */
+
+#include "wali/Common.hpp"
+#include "wali/Worklist.hpp"
+#include "wali/wfa/Trans.hpp"
+#include <map>
+#include <set>
+
+namespace wali
+{
+
+    /*!
+     * @ class StackSymLT
+     *
+     * Implements comparator to use a set for sorting transitions
+     * according in a priority queue.
+     */
+    struct StackSymLT {
+        bool operator()( const wfa::Trans* a, const wfa::Trans* b) const;
+    };
+
+    class PriorityWorklist : public Worklist
+    {
+        public:
+            typedef std::multiset< wfa::Trans*, StackSymLT > pwl_t;
+
+        public:
+            PriorityWorklist();
+            virtual ~PriorityWorklist();
+
+            virtual void put( wfa::Trans *t );
+
+            virtual wfa::Trans * get();
+
+            virtual bool empty() const;
+
+            virtual void clear();
+
+        protected:
+            pwl_t workset;
+
+    }; // class PriorityWorklist
+
+} // namespace wali
+
+#endif  // wali_PRIORITY_WORKLIST_GUARD
+
+/* Yo, Emacs!
+   ;;; Local Variables: ***
+   ;;; tab-width: 4 ***
+   ;;; End: ***
+*/
+
