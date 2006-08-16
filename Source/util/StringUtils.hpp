50df9daae649e854f81fde84d258a5b33d4be2da
Nick Kidd <kidd@cs.wisc.edu>
2006-08-16 19:46:28 +0000

    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@222 dfa36f5b-b2fa-0310-9605-e5aca7e64462

---
+++ Source/util/StringUtils.hpp
@@ -0,0 +1,26 @@
+#ifndef STRING_UTILS_GUARD
+#define STRING_UTILS_GUARD 1
+
+/*!
+ * @author Nick Kidd
+ */
+
+#include <string>
+#include <vector>
+
+class StringUtils
+{
+    public:
+        static void Tokenize(
+                const std::string& str,
+                std::vector<std::string>& tokens,
+                const std::string& delimiters );
+};
+
+#endif  // STRING_UTILS_GUARD
+
+/* Yo, Emacs!
+   ;;; Local Variables: ***
+   ;;; tab-width: 4 ***
+   ;;; End: ***
+*/
