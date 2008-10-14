8f49db56448f84d47c7ffa29b74d9408e834f45e
Nick Kidd <kidd@cs.wisc.edu>
2008-10-14 20:33:10 +0000

    Update the mirror link to be correct. Have get_parse_libs download xerces-c 3.0. This has not been tested, but everything is compiling. On to regression testing...
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@454 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- AddOns/Parse/ThirdParty/get_parse_libs.py
+++ AddOns/Parse/ThirdParty/get_parse_libs.py
@@ -5,13 +5,15 @@ import urllib
 import sys
 import os
 
-xerces_master = 'http://www.apache.org/dist/xerces/c/binaries/'
-xerces_mirror = 'http://mir2.ovh.net/ftp.apache.org/dist/xerces/c/binaries/'
-
+xerces_master = 'http://www.apache.org/dist/xerces/c/2/binaries/'
 xerces_win32   = 'xerces-c_2_8_0-x86-windows-vc_8_0'
 xerces_win64   = 'xerces-c_2_8_0-x86_64-windows-vc_8_0'
 xerces_linux32 = 'xerces-c_2_8_0-x86-linux-gcc_3_4'
 xerces_linux64 = 'xerces-c_2_8_0-x86_64-linux-gcc_3_4'
+
+xerces3_mirror = 'http://apache.cs.utah.edu/xerces/c/3/binaries/'
+xerces3_linux32 = 'xerces-c-3.0.0-x86-linux-gcc-3.4'
+xerces3_linux64 = 'xerces-c-3.0.0-x86_64-linux-gcc-3.4'
 
 def myrm(name):
     if os.path.isdir(name):
@@ -21,24 +23,21 @@ def myrm(name):
 
 def xerces_setup(name):
     myrm(name)
-    myrm(name+'.zip')
     myrm('xerces-c')
 
 def get_xerces_win(bits):
-    name =  xerces_win32 
-    if (bits == '32bit'): 
-    	name = xerces_win64
-    xerces_setup(name)
+    name =  (xerces3_win64,xerces3_win32)[bits == '32bit'] 
     target = name+'.zip'
-    os.system('curl -O %s%s' % (xerces_mirror,target))
+    xerces_setup(target)
+    os.system('curl -O %s%s' % (xerces3_mirror,target))
     os.system('unzip -o %s' % target)
     os.rename(name, 'xerces-c')
 
 def get_xerces_linux(bits):
-    name = (xerces_linux64 , xerces_linux32)[bits == '32bit']
-    xerces_setup(name)
+    name = (xerces3_linux64 , xerces3_linux32)[bits == '32bit']
     target = name+'.tar.gz'
-    os.system('curl -O %s%s' % (xerces_mirror,target))
+    xerces_setup(target)
+    os.system('curl -O %s%s' % (xerces3_mirror,target))
     os.system('tar zxf %s' % target)
     os.rename(name, 'xerces-c')
 
