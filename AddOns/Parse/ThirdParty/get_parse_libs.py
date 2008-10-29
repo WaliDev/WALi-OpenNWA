99192d161153703e17f932301f3b1003b7817ce1
Nick Kidd <kidd@cs.wisc.edu>
2008-10-29 15:49:22 +0000

    Clean up builds so that one can toggle making dir64 in base SConstruct.
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@471 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- AddOns/Parse/ThirdParty/get_parse_libs.py
+++ AddOns/Parse/ThirdParty/get_parse_libs.py
@@ -1,61 +1,58 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 ## ###############
 ## @author kidd
 
-import platform
-import urllib
-import sys
 import os
+import platform
+import shutil
+import sys
 
 
 #xerces_master   = 'http://www.apache.org/dist/xerces/c/2/binaries/'
 #
 #xerces_win32    = 'xerces-c_2_8_0-x86-windows-vc_8_0'
 #xerces_win64    = 'xerces-c_2_8_0-x86_64-windows-vc_8_0'
-#xerces_linux32  = 'xerces-c_2_8_0-x86-linux-gcc_3_4'
-#xerces_linux64  = 'xerces-c_2_8_0-x86_64-linux-gcc_3_4'
+#xerces_lin32  = 'xerces-c_2_8_0-x86-lin-gcc_3_4'
+#xerces_lin64  = 'xerces-c_2_8_0-x86_64-lin-gcc_3_4'
 
-xrcs3_mirror    = 'http://apache.cs.utah.edu/xerces/c/3/binaries/'
-xrcs3_win32     = 'xerces-c_3_0_0-x86-windows-vc_9_0'
-xrcs3_win64     = 'xerces-c_3_0_0-x86_64-windows-vc_9_0'
-xrcs3_linux32   = 'xerces-c-3.0.0-x86-linux-gcc-3.4'
-xrcs3_linux64   = 'xerces-c-3.0.0-x86_64-linux-gcc-3.4'
+xrcs3_mirror  = 'http://apache.cs.utah.edu/xerces/c/3/binaries/'
+xrcs3_win32   = 'xerces-c_3_0_0-x86-windows-vc_9_0'
+xrcs3_win64   = 'xerces-c_3_0_0-x86_64-windows-vc_9_0'
+xrcs3_lin32   = 'xerces-c-3.0.0-x86-linux-gcc-3.4'
+xrcs3_lin64   = 'xerces-c-3.0.0-x86_64-linux-gcc-3.4'
 
 (bits,linkage)  = platform.architecture()
 sys             = platform.system()
-name = ((xrcs3_win64,xrcs3_win32), (xrcs3_linux64 , xrcs3_linux32))[sys == 'Linux'][bits == '32bit']
+IsLinux         = sys == 'Linux'
+Is64            = bits == '32bit'
+name = (
+    ( xrcs3_win64 , xrcs3_win32 ), 
+    ( xrcs3_lin64 , xrcs3_lin32 )
+    )[IsLinux][Is64]
 
 def myrm(name):
-    if os.path.isdir(name):
-        os.system('rm -rf %s' % name)
-    elif os.path.isfile(name):
-        os.remove(name)
+  if os.path.isdir(name):
+    os.system('rm -rf %s' % name)
+  elif os.path.isfile(name):
+    os.remove(name)
 
-def xerces_setup(name):
-    myrm(name)
-    myrm(XercesTgt)
-
-def get_xerces_win(XercesTgt):
-    target = name+'.zip'
-    xerces_setup(target)
+def getlibs(LibInstallDir):
+  built = []
+  if False == os.path.exists(name):
+    target = name+('.zip','.tar.gz')[IsLinux]
+    myrm(target)
     os.system('curl -O %s%s' % (xrcs3_mirror,target))
-    os.system('unzip -o %s' % target)
-    os.rename(name, XercesTgt)
-
-def get_xerces_linux(XercesTgt):
-    target = name+'.tar.gz'
-    xerces_setup(target)
-    os.system('curl -O %s%s' % (xrcs3_mirror,target))
-    os.system('tar zxf %s' % target)
-    os.rename(name, XercesTgt)
-
-def getlibs(XercesTgt):
-  if False == os.path.exists(XercesTgt):
-    if s == 'Linux':
-      get_xerces_linux(XercesTgt)
-    else:
-      get_xerces_win(XercesTgt)
+    os.system('%s %s' % (('unzip -o','tar zxf')[IsLinux],target))
+  cwd = os.getcwd()
+  for lib in os.listdir(os.path.join(name,'lib')):
+    src = os.path.join(cwd,name,'lib',lib)
+    dst = os.path.join(LibInstallDir,lib)
+    print 'Src = %s' % src
+    print 'Dst = %s' % dst
+    shutil.copy2(src,dst)
+    built += [dst]
+  return (name,built)
 
 
 # Work from command line or SConscript
@@ -63,7 +60,6 @@ if __name__ in ['__main__']:
   if bits != '64bit' and bits != '32bit':
     raise NameError, bits
   Is64           = (False,True)[bits == '64bit']
-  XercesTgt      = ('xerces-c','xerces-c64')[Is64]
-  getlibs(XercesTgt)
+  getlibs(LibInstallDir)
 
 
