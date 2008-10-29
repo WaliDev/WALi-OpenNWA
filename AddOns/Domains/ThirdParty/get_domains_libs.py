0768d16ffa682fb69762cfe5adbf59abb38bdd14
Nick Kidd <kidd@cs.wisc.edu>
2008-10-29 14:29:11 +0000

    Make get_libs be configurable from SConscript for 64bit support. Current plan is to disable all 64bit options b/c I think it is probably better to just have two separate directory trees for doing 32 v 64 bit computing.
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@470 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- AddOns/Domains/ThirdParty/get_domains_libs.py
+++ AddOns/Domains/ThirdParty/get_domains_libs.py
@@ -5,32 +5,31 @@ import urllib
 import sys
 import os
 import shutil
-(bits,linkage) = platform.architecture()
-Is64           = (False,True)[bits == '64bit']
-WALiDir        = os.environ['WALiDir']
-LibInstallDir  = os.path.join(WALiDir,('lib','lib64')[Is64])
-BinInstallDir  = os.path.join(WALiDir,('bin','bin64')[Is64])
 
-def get_targz(base):
+def get_targz(base,libdir,bindir):
   wget = "curl -O"
   targz = base + '.tar.gz'
   if not os.path.isfile(targz):
       os.system('%s http://pages.cs.wisc.edu/~kidd/dolby/%s' % (wget,targz))
       os.system('tar zxvf %s' % targz)
       cwd = os.getcwd()
-      bindir = os.path.join(WALiDir,'bin')
       os.system('cd %s; ./configure --prefix=%s --libdir=%s --bindir=%s && make -j2 && make install; cd -' % 
-          (base,cwd,LibInstallDir,BinInstallDir))
+          (base,cwd,libdir,bindir))
 
-def get_buddy():
-  get_targz('buddy-2.4')
+def get_buddy(libdir,bindir):
+  get_targz('buddy-2.4',libdir,bindir)
   # Copy over the file for Visual Studio
   if platform.system() == 'Windows':
       shutil.copyfile('buddy-2.4-config.h', 'buddy-2.4/src/config.h')
 
-def getlibs():
-  get_buddy()
+def getlibs(libdir,bindir):
+  get_buddy(libdir,bindir)
 
 if __name__ in ['__main__']:
-  getlibs()
+  (bits,linkage) = platform.architecture()
+  Is64           = (False,True)[bits == '64bit']
+  cwd            = os.getcwd()
+  LibInstallDir  = os.path.join(cwd,('lib','lib64')[Is64])
+  BinInstallDir  = os.path.join(cwd,('bin','bin64')[Is64])
+  getlibs(LibInstallDir,BinInstallDir)
 
--- AddOns/Parse/ThirdParty/get_parse_libs.py
+++ AddOns/Parse/ThirdParty/get_parse_libs.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python
 
+## ###############
+## @author kidd
+
 import platform
 import urllib
 import sys
 import os
-(bits,linkage) = platform.architecture()
-Is64           = (False,True)[bits == '64bit']
-WALiDir        = os.environ['WALiDir']
-LibInstallDir  = os.path.join(WALiDir,('lib','lib64')[Is64])
-BinInstallDir  = os.path.join(WALiDir,('bin','bin64')[Is64])
-XercesTgt      = ('xerces-c','xerces-c64')[Is64]
 
-xerces_master = 'http://www.apache.org/dist/xerces/c/2/binaries/'
-xerces_win32   = 'xerces-c_2_8_0-x86-windows-vc_8_0'
-xerces_win64   = 'xerces-c_2_8_0-x86_64-windows-vc_8_0'
-xerces_linux32 = 'xerces-c_2_8_0-x86-linux-gcc_3_4'
-xerces_linux64 = 'xerces-c_2_8_0-x86_64-linux-gcc_3_4'
 
-xerces3_mirror = 'http://apache.cs.utah.edu/xerces/c/3/binaries/'
-xerces3_linux32 = 'xerces-c-3.0.0-x86-linux-gcc-3.4'
-xerces3_linux64 = 'xerces-c-3.0.0-x86_64-linux-gcc-3.4'
+#xerces_master   = 'http://www.apache.org/dist/xerces/c/2/binaries/'
+#
+#xerces_win32    = 'xerces-c_2_8_0-x86-windows-vc_8_0'
+#xerces_win64    = 'xerces-c_2_8_0-x86_64-windows-vc_8_0'
+#xerces_linux32  = 'xerces-c_2_8_0-x86-linux-gcc_3_4'
+#xerces_linux64  = 'xerces-c_2_8_0-x86_64-linux-gcc_3_4'
+
+xrcs3_mirror    = 'http://apache.cs.utah.edu/xerces/c/3/binaries/'
+xrcs3_win32     = 'xerces-c_3_0_0-x86-windows-vc_9_0'
+xrcs3_win64     = 'xerces-c_3_0_0-x86_64-windows-vc_9_0'
+xrcs3_linux32   = 'xerces-c-3.0.0-x86-linux-gcc-3.4'
+xrcs3_linux64   = 'xerces-c-3.0.0-x86_64-linux-gcc-3.4'
+
+(bits,linkage)  = platform.architecture()
+sys             = platform.system()
+name = ((xrcs3_win64,xrcs3_win32), (xrcs3_linux64 , xrcs3_linux32))[sys == 'Linux'][bits == '32bit']
 
 def myrm(name):
     if os.path.isdir(name):
@@ -31,49 +36,34 @@ def xerces_setup(name):
     myrm(name)
     myrm(XercesTgt)
 
-def get_xerces_win(bits):
-    name =  (xerces3_win64,xerces3_win32)[bits == '32bit'] 
+def get_xerces_win(XercesTgt):
     target = name+'.zip'
     xerces_setup(target)
-    os.system('curl -O %s%s' % (xerces3_mirror,target))
+    os.system('curl -O %s%s' % (xrcs3_mirror,target))
     os.system('unzip -o %s' % target)
     os.rename(name, XercesTgt)
 
-def get_xerces_linux(bits):
-    name = (xerces3_linux64 , xerces3_linux32)[bits == '32bit']
+def get_xerces_linux(XercesTgt):
     target = name+'.tar.gz'
     xerces_setup(target)
-    os.system('curl -O %s%s' % (xerces3_mirror,target))
+    os.system('curl -O %s%s' % (xrcs3_mirror,target))
     os.system('tar zxf %s' % target)
     os.rename(name, XercesTgt)
 
-def getlibs():
-    if bits != '64bit' and bits != '32bit':
-        raise NameError, bits
-    s = platform.system()
-    if False == os.path.exists(XercesTgt):
-        if s == 'Linux':
-            get_xerces_linux(bits)
-        else:
-            get_xerces_win(bits)
+def getlibs(XercesTgt):
+  if False == os.path.exists(XercesTgt):
+    if s == 'Linux':
+      get_xerces_linux(XercesTgt)
+    else:
+      get_xerces_win(XercesTgt)
 
 
 # Work from command line or SConscript
 if __name__ in ['__main__']:
-    getlibs()
+  if bits != '64bit' and bits != '32bit':
+    raise NameError, bits
+  Is64           = (False,True)[bits == '64bit']
+  XercesTgt      = ('xerces-c','xerces-c64')[Is64]
+  getlibs(XercesTgt)
 
-#def get_xerces_py(name):
-#    xerces_setup(name)
-#    print '  Fetching : %s' % link
-#    tick = [0]
-#    def reporthook(blocks, block_size, file_size): 
-#        xfer[0] += 1
-#        tmp = tick[0] + 1
-#        if tmp > 10:
-#            print '#',
-#            sys.stdout.flush()
-#            tick[0] = 0
-#        else:
-#            tick[0] = tmp
-#    urllib.urlretrieve( link , targz , reporthook );
 
