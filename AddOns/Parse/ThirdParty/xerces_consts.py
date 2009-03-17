4a42c875c61b8a7c624e900db0002fdace4cb5d9
Nick Kidd <kidd@cs.wisc.edu>
2009-03-17 20:22:50 +0000

    Xerces moved to 3.0.1, and the 3.0.0 binary packages are no longer available :(
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@519 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- AddOns/Parse/ThirdParty/xerces_consts.py
+++ AddOns/Parse/ThirdParty/xerces_consts.py
@@ -7,10 +7,10 @@
 ## found by other apps.
 import platform
 
-xrcs3_win32   = 'xerces-c-3.0.0-x86-windows-vc-9.0'
-xrcs3_win64   = 'xerces-c-3.0.0-x86_64-windows-vc-9.0'
-xrcs3_lin32   = 'xerces-c-3.0.0-x86-linux-gcc-3.4'
-xrcs3_lin64   = 'xerces-c-3.0.0-x86_64-linux-gcc-3.4'
+xrcs3_win32   = 'xerces-c-3.0.1-x86-windows-vc-9.0'
+xrcs3_win64   = 'xerces-c-3.0.1-x86_64-windows-vc-9.0'
+xrcs3_lin32   = 'xerces-c-3.0.1-x86-linux-gcc-3.4'
+xrcs3_lin64   = 'xerces-c-3.0.1-x86_64-linux-gcc-3.4'
 
 (bits,linkage)  = platform.architecture()
 sys             = platform.system()
