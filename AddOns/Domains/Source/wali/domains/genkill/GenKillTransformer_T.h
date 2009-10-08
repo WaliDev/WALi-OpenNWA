936ec06c539cc32dbaefbe7d1092af98532e9744
Junghee Lim <junghee@cs.wisc.edu>
2009-10-08 17:45:48 +0000

    GenKillTransformer_T.h: split Diff function to DiffForGen and DiffForKill
    git-svn-id: file:///p/wpis/TransformerSpecLang/Repositories/main/trunk@2550 7129bf4e-d572-4e97-9915-3212d2c624dc

--- AddOns/Domains/Source/wali/domains/genkill/GenKillTransformer_T.h
+++ AddOns/Domains/Source/wali/domains/genkill/GenKillTransformer_T.h
@@ -29,7 +29,7 @@ class Set {
 
         static bool Eq( const Set& x, const Set& y );
 
-        static Set Diff( const Set& x, const Set& y );
+        static Set DiffForGen( const Set& x, const Set& y );
 
         static Set Union( const Set& x, const Set& y );
 
@@ -52,277 +52,271 @@ using wali::SemElem;
 using wali::sem_elem_t;
 
 template< typename Set > class GenKillTransformer_T : public wali::SemElem {
+public: // methods
 
-    public: // methods
+  sem_elem_t one()    const { return MkOne();    }
+  sem_elem_t zero()   const { return MkZero();   }
+  sem_elem_t bottom() const { return MkBottom(); }
 
-        sem_elem_t one() const { return MkOne(); }
-        sem_elem_t zero() const { return MkZero(); }
-        sem_elem_t bottom() const { return MkBottom(); }
+  // A client uses makeGenKillTransformer_T to create a
+  // GenKillTransformer_T instead of calling the constructor directly;
+  // makeGenKillTransformer_T maintains unique representatives for the
+  // special semiring values one and bottom.
+  static sem_elem_t makeGenKillTransformer_T(const Set& k, const Set& g )
+  {
+      Set k_normalized = Set::DiffForKill(k,g); 
+      if (Set::Eq(k_normalized, Set::EmptySet())&& 
+              Set::Eq(g, Set::UniverseSet()))
+      {
+          return MkBottom();
+      }
+      else if (Set::Eq(k_normalized, Set::EmptySet()) && 
+              Set::Eq(g, Set::EmptySet()))
+      {
+          return MkOne();
+      }
+      else {
+          return new GenKillTransformer_T(k_normalized, g);
+      }
+  }
 
-        // A client uses makeGenKillTransformer_T to create a
-        // GenKillTransformer_T instead of calling the constructor directly;
-        // makeGenKillTransformer_T maintains unique representatives for the
-        // special semiring values one and bottom.
-        static sem_elem_t makeGenKillTransformer_T(const Set& k, const Set& g )
-        {
-            Set k_normalized = Set::Diff2(k,g); // Diff2
-            if (Set::Eq(k_normalized, Set::EmptySet())&& 
-                    Set::Eq(g, Set::UniverseSet()))
-            {
-                return MkBottom();
-            }
-            else if (Set::Eq(k_normalized, Set::EmptySet()) && 
-                    Set::Eq(g, Set::EmptySet()))
-            {
-                return MkOne();
-            }
-            else {
-                return new GenKillTransformer_T(k_normalized, g);
-            }
-        }
+  ~GenKillTransformer_T() {}
 
-        ~GenKillTransformer_T() {}
+  //-------------------------------------------------
+  // Semiring methods
+  //-------------------------------------------------
+  static sem_elem_t MkOne()
+  {
+      // Uses a method-static variable to avoid
+      // problems with static-initialization order
+      //static GenKillTransformer_T* ONE =
+      //    new GenKillTransformer_T(Set::UniverseSet(),Set::EmptySet(),1);
+      GenKillTransformer_T* ONE =
+          new GenKillTransformer_T(Set::UniverseSet(),Set::EmptySet());
+      return ONE;
+  }
 
-        //-------------------------------------------------
-        // Semiring methods
-        //-------------------------------------------------
-        static sem_elem_t MkOne()
-        {
-            // Uses a method-static variable to avoid
-            // problems with static-initialization order
-            //static GenKillTransformer_T* ONE =
-            //    new GenKillTransformer_T(Set::EmptySet(),Set::EmptySet(),1);
-            //GenKillTransformer_T* ONE =
-            //    new GenKillTransformer_T(Set::EmptySet(),Set::EmptySet());
-            GenKillTransformer_T* ONE =
-                new GenKillTransformer_T(Set::UniverseSet(),Set::EmptySet());
-            return ONE;
-        }
+  // Zero is a special value that doesn't map to any gen/kill pair,
+  // so all we really want out of this is a unique representative.
+  // The gen/kill sets with which it is initialized are arbitrary.
+  static sem_elem_t MkZero()
+  {
+      // Uses a method-static variable to avoid
+      // problems with static-initialization order
+      //static GenKillTransformer_T* ZERO =
+      //    new GenKillTransformer_T(Set::EmptySet(),Set::EmptySet(),1);
+      GenKillTransformer_T* ZERO =
+          new GenKillTransformer_T(Set::EmptySet(),Set::EmptySet());
+      return ZERO;
+  }
 
-        // Zero is a special value that doesn't map to any gen/kill pair,
-        // so all we really want out of this is a unique representative.
-        // The gen/kill sets with which it is initialized are arbitrary.
-        static sem_elem_t MkZero()
-        {
-            // Uses a method-static variable to avoid
-            // problems with static-initialization order
-            //static GenKillTransformer_T* ZERO =
-            //    new GenKillTransformer_T(Set::UniverseSet(),Set::EmptySet(),1);
-            //GenKillTransformer_T* ZERO =
-            //    new GenKillTransformer_T(Set::UniverseSet(),Set::EmptySet());
-            GenKillTransformer_T* ZERO =
-                new GenKillTransformer_T(Set::EmptySet(),Set::EmptySet());
-            return ZERO;
-        }
+  static sem_elem_t MkBottom()
+  {
+      // Uses a method-static variable to avoid
+      // problems with static-initialization order
+      //static GenKillTransformer_T* BOTTOM = 
+      //    new GenKillTransformer_T(Set::EmptySet(),Set::UniverseSet(),1);
+      GenKillTransformer_T* BOTTOM = 
+          new GenKillTransformer_T(Set::EmptySet(),Set::UniverseSet());
+      return BOTTOM;
+  }
 
-        static sem_elem_t MkBottom()
-        {
-            // Uses a method-static variable to avoid
-            // problems with static-initialization order
-            //static GenKillTransformer_T* BOTTOM = 
-            //    new GenKillTransformer_T(Set::EmptySet(),Set::UniverseSet(),1);
-            GenKillTransformer_T* BOTTOM = 
-                new GenKillTransformer_T(Set::EmptySet(),Set::UniverseSet());
-            return BOTTOM;
-        }
+  //
+  // extend
+  //
+  // Return the extend of x (this) and y.
+  // Considering x and y as functions, x extend y = y o x,
+  // where (g o f)(v) = g(f(v)).
+  //
+  sem_elem_t extend( SemElem* _y )
+  {
+      // Handle special case for either argument being zero()
+      if( this->equal(zero()) ) {
+          return zero();
+      }
+      else if( _y->equal(zero()) ) {
+          return zero();
+      }
+      // Handle special case for either argument being one().
+      if( this->equal(one()) ) {
+          return _y;//new GenKillTransformer_T(*y);
+      }
+      else if( _y->equal(one()) ) {
+          return new GenKillTransformer_T(*this);
+      }
 
-        //
-        // extend
-        //
-        // Return the extend of x (this) and y.
-        // Considering x and y as functions, x extend y = y o x,
-        // where (g o f)(v) = g(f(v)).
-        //
-        sem_elem_t extend( SemElem* _y )
-        {
-            // Handle special case for either argument being zero()
-            if( this->equal(zero()) ) {
-                return zero();
-            }
-            else if( _y->equal(zero()) ) {
-                return zero();
-            }
-            // Handle special case for either argument being one().
-            if( this->equal(one()) ) {
-                return _y;//new GenKillTransformer_T(*y);
-            }
-            else if( _y->equal(one()) ) {
-                return new GenKillTransformer_T(*this);
-            }
+      const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y);
 
-            const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y);
+      // Handle the general case
+      Set temp_k( Set::Union( kill, y->kill ) );
+      Set temp_g( Set::Union( Set::DiffForGen(gen,y->kill), y->gen) );
+      return makeGenKillTransformer_T( temp_k,temp_g );
+  }
 
-            // Handle the general case
-            Set temp_k( Set::Union( kill, y->kill ) );
-            Set temp_g( Set::Union( Set::Diff(gen,y->kill),y->gen) );
-            //Set temp_g( Set::Union( Set::Diff2(gen,y->kill),y->gen) );
-            return makeGenKillTransformer_T( temp_k,temp_g );
-        }
+  sem_elem_t combine( SemElem* _y )
+  {
+      // Handle special case for either argument being zero()
+      if( this->equal(zero()) ) {
+          return _y;
+      }
+      else if( _y->equal(zero()) ) {
+          return new GenKillTransformer_T(*this);
+      }
 
-        sem_elem_t combine( SemElem* _y )
-        {
-            // Handle special case for either argument being zero()
-            if( this->equal(zero()) ) {
-                return _y;
-            }
-            else if( _y->equal(zero()) ) {
-                return new GenKillTransformer_T(*this);
-            }
+      const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y);
+      // Handle the general case
+      Set temp_k( Set::Intersect( kill, y->kill ) );
+      Set temp_g( Set::Union( gen, y->gen ) );
 
-            const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y);
-            // Handle the general case
-            Set temp_k( Set::Intersect( kill, y->kill ) );
-            Set temp_g( Set::Union( gen, y->gen ) );
+      return makeGenKillTransformer_T( temp_k,temp_g );
+  }
 
-            return makeGenKillTransformer_T( temp_k,temp_g );
-        }
+  sem_elem_t quasiOne() const
+  {
+      return one();
+  }
 
-        sem_elem_t quasiOne() const
-        {
-            return one();
-        }
+  //
+  // diff(GenKillTransformer_T* y)
+  //
+  // Return the difference between x (this) and y.
+  //
+  // The return value r has two properties:
+  // 1. r ]= x,
+  //    i.e., isEqual(combine(x,r), x) == true
+  // 2. y combine r = y combine a,
+  //    i.e., isEqual(combine(y,r), combine(y,a)) == true
+  //
+  sem_elem_t diff( SemElem* _y )
+  {
+      // Handle special case for either argument being zero
+      if( this->equal(zero()) ) {
+          return zero();
+      }
+      else if( _y->equal(zero()) ) {
+          return new GenKillTransformer_T(*this);
+      }
 
-        //
-        // diff(GenKillTransformer_T* y)
-        //
-        // Return the difference between x (this) and y.
-        //
-        // The return value r has two properties:
-        // 1. r ]= x,
-        //    i.e., isEqual(combine(x,r), x) == true
-        // 2. y combine r = y combine a,
-        //    i.e., isEqual(combine(y,r), combine(y,a)) == true
-        //
-        sem_elem_t diff( SemElem* _y )
-        {
-            // Handle special case for either argument being zero
-            if( this->equal(zero()) ) {
-                return zero();
-            }
-            else if( _y->equal(zero()) ) {
-                return new GenKillTransformer_T(*this);
-            }
+      const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y);
+      // Both *this and *y are proper (non-zero) values
 
-            const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y);
-            // Both *this and *y are proper (non-zero) values
+      Set temp_k( Set::DiffForKill(Set::UniverseSet(),Set::DiffForKill(y->kill,kill)) ); 
+      Set temp_g( Set::DiffForGen(gen,y->gen) ); 
 
-            Set temp_k( Set::Diff2(Set::UniverseSet(),Set::Diff2(y->kill,kill)) ); // Diff2
-            Set temp_g( Set::Diff(gen,y->gen) ); // Diff
+      // Test for whether zero should be returned,
+      // i.e., if *this >= *y.
+      if (Set::Eq(temp_k, Set::UniverseSet()) && 
+          Set::Eq(temp_g, Set::EmptySet()))
+      {
+          return zero();
+      }
 
-            // Test for whether zero should be returned,
-            // i.e., if *this >= *y.
-            if (Set::Eq(temp_k, Set::UniverseSet()) && 
-                Set::Eq(temp_g, Set::EmptySet()))
-            {
-                return zero();
-            }
+      // Handle the general case
+      return makeGenKillTransformer_T(temp_k, temp_g);
+  }
 
-            // Handle the general case
-            return makeGenKillTransformer_T(temp_k, temp_g);
-        }
+  // Zero is a special representative that must be compared
+  // by address rather by its contained Gen/Kill sets.
+  bool isEqual(const GenKillTransformer_T* y) const
+  {
+      const GenKillTransformer_T* _zero = dynamic_cast<const GenKillTransformer_T*>(zero().get_ptr());
+      const GenKillTransformer_T* _one = dynamic_cast<const GenKillTransformer_T*>(one().get_ptr());
 
-        // Zero is a special representative that must be compared
-        // by address rather by its contained Gen/Kill sets.
-        bool isEqual(const GenKillTransformer_T* y) const
-        {
-            const GenKillTransformer_T* _zero = dynamic_cast<const GenKillTransformer_T*>(zero().get_ptr());
-            const GenKillTransformer_T* _one = dynamic_cast<const GenKillTransformer_T*>(one().get_ptr());
-  
-            if ( this == _one && y == _one )
-                return true;
-            if ( this == _zero && y == _zero )
-                return true;
+      if ( this == _one && y == _one )
+          return true;
+      if ( this == _zero && y == _zero )
+          return true;
 
-            return Set::Eq(kill,y->kill) && Set::Eq(gen,y->gen);
-        }
+      return Set::Eq(kill,y->kill) && Set::Eq(gen,y->gen);
+  }
 
-        bool equal(SemElem* _y) const {
-          const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y);
-          return this->isEqual(y);
-        }
+  bool equal(SemElem* _y) const {
+    const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y);
+    return this->isEqual(y);
+  }
 
-        bool equal(sem_elem_t _y) const {
-          const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y.get_ptr());
-          return this->isEqual(y);
-        }
+  bool equal(sem_elem_t _y) const {
+    const GenKillTransformer_T* y = dynamic_cast<const GenKillTransformer_T*>(_y.get_ptr());
+    return this->isEqual(y);
+  }
 
-        std::ostream& print( std::ostream& o ) const {
-            o << "<\\S.(S - {" << kill << "}) U {" << gen << "}>";
-            return o;
-        }
+  std::ostream& print( std::ostream& o ) const {
+      o << "<\\S.(S - {" << kill << "}) U {" << gen << "}>";
+      return o;
+  }
 
-        std::ostream& prettyPrint( std::ostream& o ) const {
-            return this->print(o);
-        }
+  std::ostream& prettyPrint( std::ostream& o ) const {
+      return this->print(o);
+  }
 
-        //-------------------------------------------------
-        // Other useful methods
-        //-------------------------------------------------
+  //-------------------------------------------------
+  // Other useful methods
+  //-------------------------------------------------
 
-        Set apply( const Set& input ) {
-            assert ( !(this == zero()) );
-            return Set::Union( Set::Diff(input,kill),gen );
-        }
+  Set apply( const Set& input ) {
+      assert ( !(this == zero()) );
+      return Set::Union( Set::DiffForGen(input,kill),gen );
+  }
 
-        Set& getKill() {
-            return kill;
-        }
+  Set& getKill() {
+      return kill;
+  }
 
-        const Set& getKill() const {
-            return kill;
-        }
+  const Set& getKill() const {
+      return kill;
+  }
 
-        Set& getGen() {
-            return gen;
-        }
+  Set& getGen() {
+      return gen;
+  }
 
-        const Set& getGen() const {
-            return gen;
-        }
+  const Set& getGen() const {
+      return gen;
+  }
 
-        static std::ostream& WPDS_CALL print_static_transformers( std::ostream& o )
-        {
-            o << "ONE\t=\t";    one()->print(o);  o << std::endl;
-            o << "ZERO\t=\t";   zero()->print(o); o << std::endl;
-            o << "BOTTOM\t=\t"; bottom()->print(o); o << std::endl;
-            return o;
-        }
+  static std::ostream& WPDS_CALL print_static_transformers( std::ostream& o )
+  {
+      o << "ONE\t=\t";    one()->print(o);  o << std::endl;
+      o << "ZERO\t=\t";   zero()->print(o); o << std::endl;
+      o << "BOTTOM\t=\t"; bottom()->print(o); o << std::endl;
+      return o;
+  }
 
-        std::ostream& WPDS_CALL print( std::ostream& o )
-        {
-            o << "GenKillTransformer\n";
-            return o;
-        }
+  std::ostream& WPDS_CALL print( std::ostream& o )
+  {
+      o << "GenKillTransformer\n";
+      return o;
+  }
 
-    private: // methods -----------------------------------------------------------
+private: // methods -----------------------------------------------------------
 
-        // Constructor
-        // The constructor is private to ensure uniqueness of one, zero, and bottom
-        GenKillTransformer_T(const Set& k, const Set& g, unsigned int c=0) :
-            kill(k), gen(g), count(c)
-        {
+  // Constructor
+  // The constructor is private to ensure uniqueness of one, zero, and bottom
+  GenKillTransformer_T(const Set& k, const Set& g, unsigned int c=0) :
+      kill(k), gen(g), count(c)
+  {
 #if 0
-            std::cerr << "GenKillTransformer_T(" << k << ", " << g << ")" << std::endl;
+      std::cerr << "GenKillTransformer_T(" << k << ", " << g << ")" << std::endl;
 #endif
-        }
-        // Constructor: JUNGHEE's addition
-        // The constructor is private to ensure uniqueness of one, zero, and bottom
-        GenKillTransformer_T(const GenKillTransformer_T& a) :
-            kill(a.kill), gen(a.gen), count(0)
-        {
+  }
+  // Constructor: JUNGHEE's addition
+  // The constructor is private to ensure uniqueness of one, zero, and bottom
+  GenKillTransformer_T(const GenKillTransformer_T& a) :
+      kill(a.kill), gen(a.gen), count(0)
+  {
 #if 0
-            std::cerr << "GenKillTransformer_T(" << a.kill << ", " << a.gen << ")" << std::endl;
+      std::cerr << "GenKillTransformer_T(" << a.kill << ", " << a.gen << ")" << std::endl;
 #endif
-        }
+  }
 
-    public:
-      GenKillTransformer_T() {}
+public:
+GenKillTransformer_T() {}
 
-    private: // members -----------------------------------------------------------
-        Set kill, gen;   // Used to represent the function \S.(S - kill) U gen
+private: // members -----------------------------------------------------------
+  Set kill, gen;   // Used to represent the function \S.(S - kill) U gen
 
-    public: // members -----------------------------------------------------------
-        RefCounter count;
+public: // members -----------------------------------------------------------
+  RefCounter count;
 };
 
 template< typename Set >
