a9b7e5db5a083f22330263ec4f25b02052960a45
Evan Driscoll <driscoll@cs.wisc.edu>
2010-08-30 16:48:27 +0000

    More project configuration changes to get it to build with WALi stuff
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/branches/burtona@796 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/driver.cpp
+++ Tests/nwa/driver.cpp
@@ -1,3 +1,11 @@
+#include "wali/nwa/NWA.hpp"
+#include <iostream>
+
+using namespace std;
+
 int main()
 {
+  wali::nwa::SymbolSet s;
+  s.addSymbol(wali::getKey("hello"));
+  cout << s.isSymbol(wali::getKey("hello")) << endl;
 }
