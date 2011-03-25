eae133c21408470ddf8a258dd3043cf20b9bcc04
Evan Driscoll <driscoll@cs.wisc.edu>
2011-03-25 22:43:41 +0000

    Added DEPRECATE_PUSH_AND_DISABLE and DEPRECATE_POP macros to disable warnings, but at least in GCC, they don't work so well. (They are too coarse-grained to be useful for me.)
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/branches/burtona@923 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Source/wali/nwa/deprecate.h
+++ Source/wali/nwa/deprecate.h
@@ -1,6 +1,7 @@
 #ifndef wali_nwa_DEPRECATE_H
 #define wali_nwa_DEPRECATE_H
 
+// Define the DEPRECATE macro
 #if defined(__GNUC__)
 #  define TEMP_GCC_VERSION (__GNUC__*10000 + __GNUC_MINOR__*100 + __GNUC_PATCHLEVEL__)
 #  if TEMP_GCC_VERSION > 40500 // if at least 4.5
@@ -12,6 +13,24 @@
 #  define DEPRECATE(msg) __declspec(deprecated(msg))
 #else
 #  warning I do not know how to deprecate something with your compiler
+#endif
+
+// Define macros to turn on and off deprecation
+#if defined(__GNUC__)
+#  define TEMP_GCC_VERSION (__GNUC__*10000 + __GNUC_MINOR__*100 + __GNUC_PATCHLEVEL__)
+#  if TEMP_GCC_VERSION > 40600 // if at least 4.6
+#    define DEPRECATE_PUSH_AND_DISABLE _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
+#    define DEPRECATE_POP              _Pragma("GCC diagnostic pop")
+#  else
+//   We can't actually push and pop in GCC < 4.6, so just assume it was on and turn it back on when done
+#    define DEPRECATE_PUSH_AND_DISABLE _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
+#    define DEPRECATE_POP              _Pragma("GCC diagnostic warning \"-Wdeprecated-declarations\"")
+#  endif
+#elif defined(_MSC_VER)
+#  define DEPRECATE_PUSH_AND_DISABLE __pragma(warning(push)) __pragma(warning(disable:4996))
+#  define DEPRECATE_POP              __pragma(warning(pop))
+#else
+#  warning I do not know how to turn on and off deprecated warnings
 #endif
 
 
