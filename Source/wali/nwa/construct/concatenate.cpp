9f4aa5d022c51b2dc2ac0259a8bb2c7897b9058d
Evan Driscoll <driscoll@cs.wisc.edu>
2011-06-08 02:49:26 +0000

    Whitespace changes (indenting to the right level)
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1116 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Source/wali/nwa/construct/concatenate.cpp
+++ Source/wali/nwa/construct/concatenate.cpp
@@ -14,65 +14,65 @@ namespace wali
         return nwa;
       }
 
-    /**
-     *
-     * @brief constructs the NWA resulting from the concatenation of the given NWAs
-     *
-     * @param - first: the NWA to which 'second' should be concatenated
-     * @param - second: the NWA to concatenate onto the end of 'first'
-     *
-     */
-    void concatenate( NWA & out, NWA const & first, NWA const & second )
-    {
-      //Q: Do we need to guarantee that there is no overlap in states between machines?
-      //A: YES!
-      //    If there is overlap, then we could conceivably go through the first half of one
-      //    machine and then skip to the other machine before completing the first machine.
-      //Q: How should we do this?  
-      //    2. Generate new keys by the pairing scheme (1,k) for keys in first and (2,k) for keys in second.
-      //    3. Implement a check for overlapping state keys and assert that no such overlap occurs.
-      //A: 3 is easiest
-
-      //Q: How should clientInfos be generated for the concatenated NWA?
-      //A: The clientInfos from the component machines are copied and added to the concatenated NWA.
-
-      //Test state Key overlap of the two NWAs.
-      assert(! NWA::overlap(first, second) );
-
-      //Clear all states(except the stuck state) and transitions from this machine.
-      out.clear();
-
-      //Duplicate all of the functionality of the first machine (except the final state property).
-      out = first;
-      out.clearFinalStates();
-
-      //Duplicate all of the functionality of the second machine (except the initial state property).
-      out.combineWith(second);
-
-
-      // Now we have both machines combined. We need to set the final states
-      // of the combined machine to be the final states of the second
-      // machine, then add epsilon transitions from the final states of the
-      // first to the final states of the second.
-
-      // First step: set final states.
-      for (NWA::stateIterator final = second.beginFinalStates();
-           final != second.endFinalStates(); ++final)
+      /**
+       *
+       * @brief constructs the NWA resulting from the concatenation of the given NWAs
+       *
+       * @param - first: the NWA to which 'second' should be concatenated
+       * @param - second: the NWA to concatenate onto the end of 'first'
+       *
+       */
+      void concatenate( NWA & out, NWA const & first, NWA const & second )
       {
-        out.addFinalState(*final);
-      }
-      
-      // Second step: add transitions from the first machine to the second
-      for( NWA::stateIterator fit = first.beginFinalStates();
-           fit != first.endFinalStates(); fit++ )
-      {
-        for( NWA::stateIterator sit = second.beginInitialStates();
-             sit != second.endInitialStates(); sit++ )
+        //Q: Do we need to guarantee that there is no overlap in states between machines?
+        //A: YES!
+        //    If there is overlap, then we could conceivably go through the first half of one
+        //    machine and then skip to the other machine before completing the first machine.
+        //Q: How should we do this?  
+        //    2. Generate new keys by the pairing scheme (1,k) for keys in first and (2,k) for keys in second.
+        //    3. Implement a check for overlapping state keys and assert that no such overlap occurs.
+        //A: 3 is easiest
+
+        //Q: How should clientInfos be generated for the concatenated NWA?
+        //A: The clientInfos from the component machines are copied and added to the concatenated NWA.
+
+        //Test state Key overlap of the two NWAs.
+        assert(! NWA::overlap(first, second) );
+
+        //Clear all states(except the stuck state) and transitions from this machine.
+        out.clear();
+
+        //Duplicate all of the functionality of the first machine (except the final state property).
+        out = first;
+        out.clearFinalStates();
+
+        //Duplicate all of the functionality of the second machine (except the initial state property).
+        out.combineWith(second);
+
+
+        // Now we have both machines combined. We need to set the final states
+        // of the combined machine to be the final states of the second
+        // machine, then add epsilon transitions from the final states of the
+        // first to the final states of the second.
+
+        // First step: set final states.
+        for (NWA::stateIterator final = second.beginFinalStates();
+             final != second.endFinalStates(); ++final)
         {
-          out.addInternalTrans(*fit, WALI_EPSILON, *sit);
+          out.addFinalState(*final);
+        }
+      
+        // Second step: add transitions from the first machine to the second
+        for( NWA::stateIterator fit = first.beginFinalStates();
+             fit != first.endFinalStates(); fit++ )
+        {
+          for( NWA::stateIterator sit = second.beginInitialStates();
+               sit != second.endInitialStates(); sit++ )
+          {
+            out.addInternalTrans(*fit, WALI_EPSILON, *sit);
+          }
         }
       }
-    }
 
       
     }
--- Source/wali/nwa/construct/reverse.cpp
+++ Source/wali/nwa/construct/reverse.cpp
@@ -14,80 +14,80 @@ namespace wali
         return nwa;
       }
 
-    /**
-     * @brief constructs the NWA which is the reverse of the given NWA
-     *
-     * @param - first: the NWA to reverse
-     * 
-     */
-    void reverse( NWA & out, NWA const & first )
-    {
-      //Q: How should clientInfos be generated for the reversed NWA?
-      //A: The clientInfos from the component machines are copied and added to the reversed NWA.
-
-      //Clear all states(except the stuck state) and transitions from this machine.
-      out.clear();
-
-
-      //The reverse machine has all the states of the original machine.
-      // FIXME-RELEASE: copy clientinfo. Or figure out how to do this.
-      for (NWA::stateIterator state = first.beginStates();
-           state != first.endStates(); ++state)
+      /**
+       * @brief constructs the NWA which is the reverse of the given NWA
+       *
+       * @param - first: the NWA to reverse
+       * 
+       */
+      void reverse( NWA & out, NWA const & first )
       {
-        out.addState(*state);
-      }
+        //Q: How should clientInfos be generated for the reversed NWA?
+        //A: The clientInfos from the component machines are copied and added to the reversed NWA.
+
+        //Clear all states(except the stuck state) and transitions from this machine.
+        out.clear();
+
+
+        //The reverse machine has all the states of the original machine.
+        // FIXME-RELEASE: copy clientinfo. Or figure out how to do this.
+        for (NWA::stateIterator state = first.beginStates();
+             state != first.endStates(); ++state)
+        {
+          out.addState(*state);
+        }
           
 
-      //Swap initial and final state functionality.
-      for( NWA::stateIterator it = first.beginInitialStates(); 
-           it != first.endInitialStates(); it++ )
-      {
-        out.addFinalState(*it);
-      }
-      for( NWA::stateIterator it = first.beginFinalStates(); 
-           it != first.endFinalStates(); it++ )
-      {
-        out.addInitialState(*it);
-      }
+        //Swap initial and final state functionality.
+        for( NWA::stateIterator it = first.beginInitialStates(); 
+             it != first.endInitialStates(); it++ )
+        {
+          out.addFinalState(*it);
+        }
+        for( NWA::stateIterator it = first.beginFinalStates(); 
+             it != first.endFinalStates(); it++ )
+        {
+          out.addInitialState(*it);
+        }
 
       
-      //Duplicate internal transitions with source/target swapped.
-      for( NWA::InternalIterator it = first.beginInternalTrans(); 
-           it != first.endInternalTrans(); it++ )
-      {
-        out.addInternalTrans(NWA::Trans::getTarget(*it),
-                             NWA::Trans::getInternalSym(*it),
-                             NWA::Trans::getSource(*it));
-      }
-
-      //Duplicate return transitions as call transitions with (return,sym,exit).
-      for( NWA::ReturnIterator it = first.beginReturnTrans(); 
-           it != first.endReturnTrans(); it++ )
-      {
-        out.addCallTrans(NWA::Trans::getReturnSite(*it),
-                         NWA::Trans::getReturnSym(*it),
-                         NWA::Trans::getExit(*it));
-      }
-
-      //Duplicate call transitions with associated return transitions as 
-      //return transitions with (entry,return,sym,call).
-      for( NWA::CallIterator cit = first.beginCallTrans(); 
-           cit != first.endCallTrans(); cit++ )
-      {
-        for( NWA::ReturnIterator rit = first.beginReturnTrans(); 
-             rit != first.endReturnTrans(); rit++ )
+        //Duplicate internal transitions with source/target swapped.
+        for( NWA::InternalIterator it = first.beginInternalTrans(); 
+             it != first.endInternalTrans(); it++ )
         {
-          if( NWA::Trans::getCallSite(*cit) == NWA::Trans::getCallSite(*rit) )
+          out.addInternalTrans(NWA::Trans::getTarget(*it),
+                               NWA::Trans::getInternalSym(*it),
+                               NWA::Trans::getSource(*it));
+        }
+
+        //Duplicate return transitions as call transitions with (return,sym,exit).
+        for( NWA::ReturnIterator it = first.beginReturnTrans(); 
+             it != first.endReturnTrans(); it++ )
+        {
+          out.addCallTrans(NWA::Trans::getReturnSite(*it),
+                           NWA::Trans::getReturnSym(*it),
+                           NWA::Trans::getExit(*it));
+        }
+
+        //Duplicate call transitions with associated return transitions as 
+        //return transitions with (entry,return,sym,call).
+        for( NWA::CallIterator cit = first.beginCallTrans(); 
+             cit != first.endCallTrans(); cit++ )
+        {
+          for( NWA::ReturnIterator rit = first.beginReturnTrans(); 
+               rit != first.endReturnTrans(); rit++ )
           {
-            out.addReturnTrans(NWA::Trans::getEntry(*cit),
-                               NWA::Trans::getReturnSite(*rit),
-                               NWA::Trans::getCallSym(*cit),
-                               NWA::Trans::getCallSite(*cit));
+            if( NWA::Trans::getCallSite(*cit) == NWA::Trans::getCallSite(*rit) )
+            {
+              out.addReturnTrans(NWA::Trans::getEntry(*cit),
+                                 NWA::Trans::getReturnSite(*rit),
+                                 NWA::Trans::getCallSym(*cit),
+                                 NWA::Trans::getCallSite(*cit));
+            }
           }
         }
-      }
 
-    }
+      }
 
       
     }
