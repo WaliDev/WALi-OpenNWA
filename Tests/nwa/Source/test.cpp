c2e8a3e9c877b97213f8fa4b2f0894a630e4a128
Evan Driscoll <driscoll@cs.wisc.edu>
2011-06-14 17:18:50 +0000

    Added some additional statistics output. To use this, you must modify the gtest code as described below.
    In Tests/nwa/ThirdParty/gtest-1.6.0:
    
       At the end of include/gtest.h (outside the namespace) put
          extern int gtest_num_test_parts;
    
       In include/gtest_pred_impl.h, modify the fourth line of GTEST_ASSERT_ (line
       75) so that the result looks like this:
          #define GTEST_ASSERT_(expression, on_failure) \
            GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
            if (const ::testing::AssertionResult gtest_ar = (expression)) \
              gtest_num_test_parts++; \
            else \
              on_failure(gtest_ar.failure_message())
    
    
    
    
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1179 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/test.cpp
+++ Tests/nwa/Source/test.cpp
@@ -1,12 +1,15 @@
+#include <iostream>
 #include "gtest/gtest.h"
 
-
-TEST(a,b) {
-    EXPECT_EQ(1,1);
-}
-
+int gtest_num_test_parts = 0;
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  int ret = RUN_ALL_TESTS();
+
+  if (gtest_num_test_parts > 0) {
+      std::cout << "There were " << gtest_num_test_parts << " assertions checked!\n";
+  }
+  
+  return ret;
 }
