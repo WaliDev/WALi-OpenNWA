cba833f4d3259a85c65404959ba1bb7eaa8d1ae5
Evan Driscoll <driscoll@cs.wisc.edu>
2011-06-23 02:45:44 +0000

    Fix a couple bugs regarding handling stuff
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1203 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Source/wali/nwa/traverse/Expand.cpp
+++ Source/wali/nwa/traverse/Expand.cpp
@@ -1,5 +1,4 @@
 #include "wali/nwa/traverse/depth.hpp"
-
 #include "wali/nwa/NWA.hpp"
 
 namespace wali {
@@ -20,6 +19,20 @@ namespace wali {
 
           return getKey(result, state);
         }
+
+        State getPrefixedName(CallString const & cs, State call, State state) {
+          State result = cs[0];
+
+          for (unsigned i=1; i<cs.size(); ++i) {
+            result = getKey(result, cs[i]);
+          }
+
+          result = getKey(result, call);
+          result = getKey(result, state);
+
+          return result;
+        }
+
         
       public:
         TransCopier(NWA & destination_nwa)
@@ -31,10 +44,9 @@ namespace wali {
                                 State source, Symbol symbol, State target)
         {
           (void) nwa;
-          source = getPrefixedName(cs, source);
-          target = getPrefixedName(cs, target);
-          
-          dest_nwa.addInternalTrans(source, symbol, target);
+          dest_nwa.addInternalTrans(getPrefixedName(cs, source),
+                                    symbol,
+                                    getPrefixedName(cs, target));
         }
 
         
@@ -42,10 +54,9 @@ namespace wali {
                             State source, Symbol symbol, State target)
         {
           (void) nwa;
-          source = getPrefixedName(cs, source);
-          target = getPrefixedName(cs, target);
-          
-          dest_nwa.addCallTrans(source, symbol, target);
+          dest_nwa.addCallTrans(getPrefixedName(cs, source),
+                                symbol,
+                                getPrefixedName(cs, source, target));
         }
 
         
@@ -53,11 +64,10 @@ namespace wali {
                               State source, State pred, Symbol symbol, State target)
         {
           (void) nwa;
-          source = getPrefixedName(cs, source);
-          pred = getPrefixedName(cs, pred);
-          target = getPrefixedName(cs, target);
-          
-          dest_nwa.addReturnTrans(source, pred, symbol, target);
+          dest_nwa.addReturnTrans(getPrefixedName(cs, pred, source),
+                                  getPrefixedName(cs, pred),
+                                  symbol,
+                                  getPrefixedName(cs, target));
         }
       };
       
