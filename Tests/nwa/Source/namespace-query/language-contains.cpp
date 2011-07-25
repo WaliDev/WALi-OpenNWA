5948ef7da7225c627192e5f9ff21696b1e524e32
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-25 22:19:48 +0000

    Added tests for languageContains. Currently the program asserts.
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1304 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/namespace-query/language-contains.cpp
+++ Tests/nwa/Source/namespace-query/language-contains.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "gtest/gtest.h"
 
 #include "wali/nwa/NWA.hpp"
@@ -8,6 +10,10 @@
 
 using namespace wali::nwa;
 
+#define NUM_ELEMENTS(array)  (sizeof(array)/sizeof((array)[0]))
+
+// WARNING: the order of these words must be consistent with the row order
+//          in the table 'expected_answers' below.
 NWA const nwas[] = {
     AcceptsBalancedOnly().nwa,
     AcceptsStrictlyUnbalancedLeft().nwa,
@@ -17,6 +23,37 @@ NWA const nwas[] = {
     AcceptsPositionallyConsistentString().nwa
 };
 
+const unsigned num_nwas = NUM_ELEMENTS(nwas);
+
+
+// WARNING: the order of these words must be consistent with the column order
+//          in the table 'expected_answers' below.
+NestedWord const words[] = {
+    WordCollection().empty,
+    WordCollection().balanced,
+    WordCollection().balanced0,
+    WordCollection().unbalancedLeft,
+    WordCollection().unbalancedLeft0,
+    WordCollection().unbalancedRight,
+    WordCollection().unbalancedRight0,
+    WordCollection().fullyUnbalanced,
+    WordCollection().fullyUnbalanced0
+};
+
+const unsigned num_words = NUM_ELEMENTS(words);
+
+
+// WARNING: the order of the rows and columns in this table must be
+//          consistent with the order of 'words' and 'nwas' above.
+bool expected_answers[][num_words] = {
+    /*                     empty    bal    bal0    left   left0    right  right0   full   full0 */
+    /* balanced only  */ { true,    true,  true,   false, false,   false, false,   false, false },
+    /* strictly left  */ { false,   false, true,   true,  true,    false, false,   false, false },
+    /* maybe left     */ { true,    true,  true,   true,  true,    false, false,   false, false },
+    /* strictly right */ { false,   false, true,   false, false,   true,  true,    false, false },
+    /* maybe right    */ { true,    true,  true,   false, false,   true,  true,    false, false },
+    /* maybe fully    */ { true,    true,  true,   true,  true,    true,  true,    true,  true  }
+};
 
 
 
@@ -24,7 +61,19 @@ namespace wali {
     namespace nwa {
         namespace query {
 
-            
+            TEST(wali$nwa$query$$languageContains, testBatteryOfVariouslyBalancedWordsAndNwas)
+            {
+                for (unsigned word = 0 ; word < num_words ; ++word) {
+                    for (unsigned nwa = 0 ; nwa < num_nwas ; ++nwa) {
+                        std::stringstream ss;
+                        ss << "Current NWA number " << nwa << " and word number " << word;
+                        SCOPED_TRACE(ss.str());
+
+                        EXPECT_EQ(expected_answers[nwa][word],
+                                  languageContains(nwas[nwa], words[word]));
+                    }
+                }
+            }
 
 
         }
