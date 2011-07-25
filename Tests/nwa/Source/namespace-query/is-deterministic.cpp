c6007f9a967fe35874cf1d028d8b952d81835a2c
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-25 19:30:02 +0000

    Added more tests for isDeterministic. One currently fails.
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1294 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/namespace-query/is-deterministic.cpp
+++ Tests/nwa/Source/namespace-query/is-deterministic.cpp
@@ -44,6 +44,58 @@ namespace wali {
 
                 EXPECT_FALSE(isDeterministic(fixture.nwa));
             }
+
+
+            TEST(wali$nwa$query$$isDeterministic, merePresenceOfWildsDoesNotMakeNondeterministic)
+            {
+                OddNumEvenGroupsNwa fixture;
+                fixture.nwa.removeState(fixture.dummy);
+
+                fixture.nwa.addInternalTrans(fixture.q0, WALI_WILD, fixture.q1);
+                fixture.nwa.addCallTrans(fixture.q3, WALI_WILD, fixture.q1);
+                fixture.nwa.addReturnTrans(fixture.q2, fixture.q3, WALI_WILD, fixture.q1);
+                // Make sure we test two wild transitions out of the same
+                // state with different call predecessors...
+                fixture.nwa.addReturnTrans(fixture.q2, fixture.q0, WALI_WILD, fixture.q1);
+                // ... and from different states with the same call predecessors
+                fixture.nwa.addReturnTrans(fixture.q1, fixture.q3, WALI_WILD, fixture.q1);
+                
+                EXPECT_TRUE(isDeterministic(fixture.nwa));
+            }
+
+            TEST(wali$nwa$query$$isDeterministic, twoWildInternalTransitionsFromSameStateMakesNondeterministic)
+            {
+                OddNumEvenGroupsNwa fixture;
+                fixture.nwa.removeState(fixture.dummy);
+
+                fixture.nwa.addInternalTrans(fixture.q0, WALI_WILD, fixture.q1);
+                fixture.nwa.addInternalTrans(fixture.q0, WALI_WILD, fixture.q2);
+
+                EXPECT_FALSE(isDeterministic(fixture.nwa));
+            }
+
+            TEST(wali$nwa$query$$isDeterministic, twoWildCallTransitionsFromSameStateMakesNondeterministic)
+            {
+                OddNumEvenGroupsNwa fixture;
+                fixture.nwa.removeState(fixture.dummy);
+
+                fixture.nwa.addCallTrans(fixture.q3, WALI_WILD, fixture.q1);
+                fixture.nwa.addCallTrans(fixture.q3, WALI_WILD, fixture.q1);
+
+                EXPECT_FALSE(isDeterministic(fixture.nwa));
+            }
+
+            TEST(wali$nwa$query$$isDeterministic, twoWildReturnTransitionsFromSameStateAndPredecessorMakesNondeterministic)
+            {
+                OddNumEvenGroupsNwa fixture;
+                fixture.nwa.removeState(fixture.dummy);
+
+                fixture.nwa.addReturnTrans(fixture.q2, fixture.q1, WALI_WILD, fixture.q3);
+                fixture.nwa.addReturnTrans(fixture.q2, fixture.q1, WALI_WILD, fixture.q0);
+
+                EXPECT_FALSE(isDeterministic(fixture.nwa));
+            }
+
         }
     }
 }
