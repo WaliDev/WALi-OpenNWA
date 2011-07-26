5815b056e744972dc6e3881221f3a27adce3d1ac
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-26 16:34:26 +0000

    Committed tests for languageIsEmpty
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1316 dfa36f5b-b2fa-0310-9605-e5aca7e64462

---
+++ Tests/nwa/Source/namespace-query/language-is-empty.cpp
@@ -0,0 +1,66 @@
+#include "gtest/gtest.h"
+
+#include "wali/nwa/NWA.hpp"
+#include "wali/nwa/query/language.hpp"
+
+#include "Tests/nwa/Source/fixtures.hpp"
+#include "Tests/nwa/Source/class-NWA/supporting.hpp"
+
+using namespace wali::nwa;
+
+#define NUM_ELEMENTS(array)  (sizeof(array)/sizeof((array)[0]))
+
+// WARNING: the order of these words must be consistent with the row & column
+//          order in the table 'expected_answers' below.
+static NWA const nwas[] = {
+    NWA(),
+    AcceptsBalancedOnly().nwa,
+    AcceptsStrictlyUnbalancedLeft().nwa,
+    AcceptsPossiblyUnbalancedLeft().nwa,
+    AcceptsStrictlyUnbalancedRight().nwa,
+    AcceptsPossiblyUnbalancedRight().nwa,
+    AcceptsPositionallyConsistentString().nwa
+};
+
+static const unsigned num_nwas = NUM_ELEMENTS(nwas);
+
+
+// WARNING: the order of the rows and columns in this table must be
+//          consistent with the order of 'words' and 'nwas' above.
+//
+// "Is the row NWA a subset(eq) of the column NWA?"
+static bool expected_answers[] = {
+    /* empty        */  true,
+    /* balanced     */  false,
+    /* strict left  */  true,
+    /* maybe left   */  false,
+    /* strict right */  true,
+    /* maybe right  */  false,
+    /* maybe full   */  false
+};
+
+
+namespace wali {
+    namespace nwa {
+        namespace query {
+
+            TEST(wali$nwa$query$$languageIsEmpty, testBatteryOfVariouslyBalancedNwas)
+            {
+                for (unsigned nwa = 0 ; nwa < num_nwas ; ++nwa) {
+                    std::stringstream ss;
+                    ss << "Testing NWA " << nwa;
+                    SCOPED_TRACE(ss.str());
+                    
+                    if (expected_answers[nwa]) {
+                        EXPECT_TRUE(languageIsEmpty(nwas[nwa]));
+                    }
+                    else {
+                        EXPECT_FALSE(languageIsEmpty(nwas[nwa]));
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
