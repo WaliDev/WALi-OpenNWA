ea64e7f07f4e9bb622db4f393f6c67d50e5b24d2
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-26 21:32:44 +0000

    Replace dumb functions with gtest TEST(...), and replace assert() with EXPECT_TRUE()
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1339 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/serialization/parser-unit-tests.cpp
+++ Tests/nwa/Source/serialization/parser-unit-tests.cpp
@@ -27,16 +27,16 @@ namespace wali {
         discardws(std::istream & is)
           ;
 
-        void
-        test_discardws()
+
+        TEST(wali$nwa$parser$$discardws, unitTests)
         {
           std::stringstream ss("  helllo");
 
-          assert(ss.peek() == ' '); // mostly just testing .peek does what I think
+          EXPECT_TRUE(ss.peek() == ' '); // mostly just testing .peek does what I think
           discardws(ss);
-          assert(ss.peek() == 'h'); // make sure we discard up to the h
+          EXPECT_TRUE(ss.peek() == 'h'); // make sure we discard up to the h
           discardws(ss);
-          assert(ss.peek() == 'h'); // make sure we discard nothing if there's no WS
+          EXPECT_TRUE(ss.peek() == 'h'); // make sure we discard nothing if there's no WS
         }
 
         extern
@@ -57,7 +57,7 @@ namespace wali {
             , read_char(c)
             , line(li)
           {
-            assert(c!=-1);
+            EXPECT_TRUE(c!=-1);
             message = err_msg();
           }
 
@@ -100,8 +100,7 @@ namespace wali {
         struct StreamTooShortException   : std::exception {};
 
 
-        void
-        test_read_lit()
+        TEST(wali$nwa$parser$$readLit, unitTests)
         {
           std::stringstream empty("");
           std::stringstream short_("he");
@@ -120,7 +119,7 @@ namespace wali {
             (void) e;
             exn = true;
           }
-          assert(exn);
+          EXPECT_TRUE(exn);
 
           exn = false;
           try {
@@ -130,12 +129,12 @@ namespace wali {
             (void) e;
             exn = true;
           }
-          assert(exn);
+          EXPECT_TRUE(exn);
 
           // Make sure if we start with the query we get it right
-          assert(read_lit(exact, query) == query);
-          assert(read_lit(long_, query) == query);
-          assert(long_.peek() == 'w'); // did it skip over WS?
+          EXPECT_TRUE(read_lit(exact, query) == query);
+          EXPECT_TRUE(read_lit(long_, query) == query);
+          EXPECT_TRUE(long_.peek() == 'w'); // did it skip over WS?
 
           // Make sure if we start with stuff other than the query we get an
           // exception
@@ -147,7 +146,7 @@ namespace wali {
             (void) e;
             exn = true;
           }
-          assert(exn);
+          EXPECT_TRUE(exn);
         }
 
         
@@ -156,8 +155,8 @@ namespace wali {
         read_name(std::istream & is)
           ;        
 
-        void
-        test_read_name()
+        
+        TEST(wali$nwa$parser$$readName, unitTests)
         {
           // Test several that look like they are a non-final name in a list
           std::stringstream ss1(" hello, world");
@@ -168,20 +167,20 @@ namespace wali {
           std::stringstream ss6(" hello (ighonetohaoey489) , world");
 
           // In every one of those, "hello" should be considered the key
-          assert(read_name(ss1) == "hello");
-          assert(read_name(ss2) == "hello");
-          assert(read_name(ss3) == "hello");
-          assert(read_name(ss4) == "hello");
-          assert(read_name(ss5) == "hello");
-          assert(read_name(ss6) == "hello");
+          EXPECT_TRUE(read_name(ss1) == "hello");
+          EXPECT_TRUE(read_name(ss2) == "hello");
+          EXPECT_TRUE(read_name(ss3) == "hello");
+          EXPECT_TRUE(read_name(ss4) == "hello");
+          EXPECT_TRUE(read_name(ss5) == "hello");
+          EXPECT_TRUE(read_name(ss6) == "hello");
 
           // And it should always remove up to but not including the comma
-          assert(ss1.peek() == ',');
-          assert(ss2.peek() == ',');
-          assert(ss3.peek() == ',');
-          assert(ss4.peek() == ',');
-          assert(ss5.peek() == ',');
-          assert(ss6.peek() == ',');
+          EXPECT_TRUE(ss1.peek() == ',');
+          EXPECT_TRUE(ss2.peek() == ',');
+          EXPECT_TRUE(ss3.peek() == ',');
+          EXPECT_TRUE(ss4.peek() == ',');
+          EXPECT_TRUE(ss5.peek() == ',');
+          EXPECT_TRUE(ss6.peek() == ',');
 
           // Test several that look like a final-one in a list
           std::stringstream ss7(" hello)");
@@ -191,20 +190,20 @@ namespace wali {
           std::stringstream ss11(" hello (o,nthent9r34<>)}");
           std::stringstream ss12(" hello (o,nthent9r34<>)\n}");
 
-          assert(read_name(ss7) == "hello");
-          assert(read_name(ss8) == "hello");
-          assert(read_name(ss9) == "hello");
-          assert(read_name(ss10) == "hello");
-          assert(read_name(ss11) == "hello");
-          assert(read_name(ss12) == "hello");
+          EXPECT_TRUE(read_name(ss7) == "hello");
+          EXPECT_TRUE(read_name(ss8) == "hello");
+          EXPECT_TRUE(read_name(ss9) == "hello");
+          EXPECT_TRUE(read_name(ss10) == "hello");
+          EXPECT_TRUE(read_name(ss11) == "hello");
+          EXPECT_TRUE(read_name(ss12) == "hello");
 
           // Make sure it stops at the appropriate delimiter
-          assert(ss7.peek() == ')');
-          assert(ss8.peek() == ')');
-          assert(ss9.peek() == ')');
-          assert(ss10.peek() == '}');
-          assert(ss11.peek() == '}');
-          assert(ss12.peek() == '}');
+          EXPECT_TRUE(ss7.peek() == ')');
+          EXPECT_TRUE(ss8.peek() == ')');
+          EXPECT_TRUE(ss9.peek() == ')');
+          EXPECT_TRUE(ss10.peek() == '}');
+          EXPECT_TRUE(ss11.peek() == '}');
+          EXPECT_TRUE(ss12.peek() == '}');
 
           // Now test a hard case: parens as the key
           std::stringstream ss13("(key1,2) bar");
@@ -213,11 +212,11 @@ namespace wali {
           std::stringstream ss16("({key1,2}) bar");
           std::stringstream ss17("({key1,2)) bar");
     
-          assert(read_name(ss13) == "(key1,2)");
-          assert(read_name(ss14) == "{key1,2}");
-          assert(read_name(ss15) == "(<key1,2>)");
-          assert(read_name(ss16) == "({key1,2})");
-          assert(read_name(ss17) == "({key1,2))");
+          EXPECT_TRUE(read_name(ss13) == "(key1,2)");
+          EXPECT_TRUE(read_name(ss14) == "{key1,2}");
+          EXPECT_TRUE(read_name(ss15) == "(<key1,2>)");
+          EXPECT_TRUE(read_name(ss16) == "({key1,2})");
+          EXPECT_TRUE(read_name(ss17) == "({key1,2))");
         }
 
         
@@ -230,9 +229,9 @@ namespace wali {
         KeyQuad
         read_quad(std::istream & is)
           ;
-        
-        void
-        test_read_triple_quad()
+
+
+        TEST(wali$nwa$parser$$readTripleQuad, unitTests)
         {
           std::stringstream ss1("(a, b, c)");
           std::stringstream ss2("(a, b, c)z");
@@ -240,15 +239,15 @@ namespace wali {
 
           KeyTriple ans3(getKey("a"), getKey("b"), getKey("c"));
     
-          assert(read_triple(ss1) == ans3);
-          assert(read_triple(ss2) == ans3);
-          assert(read_triple(ss3) == ans3);
+          EXPECT_TRUE(read_triple(ss1) == ans3);
+          EXPECT_TRUE(read_triple(ss2) == ans3);
+          EXPECT_TRUE(read_triple(ss3) == ans3);
     
           std::stringstream ss4("(a (antohe), b (tnoehn), c (otnhento))z");
           std::stringstream ss5("(a (jab3,9030), b,\nc)\n}");
 
-          assert(read_triple(ss4) == ans3);
-          assert(read_triple(ss5) == ans3);
+          EXPECT_TRUE(read_triple(ss4) == ans3);
+          EXPECT_TRUE(read_triple(ss5) == ans3);
     
           std::stringstream ss6("(a, b, c, d)");
           std::stringstream ss7("(a, b, c, d)z");
@@ -256,15 +255,15 @@ namespace wali {
 
           KeyQuad ans4(getKey("a"), getKey("b"), getKey("c"), getKey("d"));
 
-          assert(read_quad(ss6) == ans4);
-          assert(read_quad(ss7) == ans4);
-          assert(read_quad(ss8) == ans4);
+          EXPECT_TRUE(read_quad(ss6) == ans4);
+          EXPECT_TRUE(read_quad(ss7) == ans4);
+          EXPECT_TRUE(read_quad(ss8) == ans4);
         
           std::stringstream ss9("(a (antohe), b (tnoehn), c (otnhento), d (otnheont349))z");
           std::stringstream ss10("(a (jab3,9030), b,\nc, d)\n}");
 
-          assert(read_quad(ss9)  == ans4);
-          assert(read_quad(ss10) == ans4);
+          EXPECT_TRUE(read_quad(ss9)  == ans4);
+          EXPECT_TRUE(read_quad(ss10) == ans4);
         }
 
 
@@ -284,27 +283,26 @@ namespace wali {
           ;
 
 
-        void
-        test_read_lists()
+        TEST(wali$nwa$parser$$readLists, unitTests)
         {
           // These should return a zero list for all three kinds of lists.  When we
-          // do the corresponding asserts below, make sure that they (really, ss2)
+          // do the corresponding EXPECT_TRUEs below, make sure that they (really, ss2)
           // is unchanged
           std::stringstream ss1("");
           std::stringstream ss2("}");
 
-          assert(read_name_list(ss1).size() == 0);
-          assert(read_name_list(ss2).size() == 0);
+          EXPECT_TRUE(read_name_list(ss1).size() == 0);
+          EXPECT_TRUE(read_name_list(ss2).size() == 0);
           ss1.str(""); // reset ss1 because C++ is dumb and 'peek()' modifes goodbit
-          assert(ss2.peek() == '}');
-          assert(read_triple_list(ss1).size() == 0);
-          assert(read_triple_list(ss2).size() == 0);
+          EXPECT_TRUE(ss2.peek() == '}');
+          EXPECT_TRUE(read_triple_list(ss1).size() == 0);
+          EXPECT_TRUE(read_triple_list(ss2).size() == 0);
           ss1.str("");
-          assert(ss2.peek() == '}');
-          assert(read_quad_list(ss1).size() == 0);
-          assert(read_quad_list(ss2).size() == 0);
+          EXPECT_TRUE(ss2.peek() == '}');
+          EXPECT_TRUE(read_quad_list(ss1).size() == 0);
+          EXPECT_TRUE(read_quad_list(ss2).size() == 0);
           ss1.str("");
-          assert(ss2.peek() == '}');
+          EXPECT_TRUE(ss2.peek() == '}');
 
           // Test read_name_list. We only test the sizes on the first four tests,
           // then test all the elements on the later ones.
@@ -315,22 +313,22 @@ namespace wali {
           std::stringstream ss7("name1, name2, name3, name4");
           std::stringstream ss8("name1, name2, name3, name4 }");
 
-          assert(read_name_list(ss3).size() == 1);
-          assert(read_name_list(ss4).size() == 1);
-          assert(read_name_list(ss5).size() == 2);
-          assert(read_name_list(ss6).size() == 2);
+          EXPECT_TRUE(read_name_list(ss3).size() == 1);
+          EXPECT_TRUE(read_name_list(ss4).size() == 1);
+          EXPECT_TRUE(read_name_list(ss5).size() == 2);
+          EXPECT_TRUE(read_name_list(ss6).size() == 2);
           std::vector<std::string> v7 = read_name_list(ss7);
-          assert(v7.size() == 4);
-          assert(v7[0] == "name1");
-          assert(v7[1] == "name2");
-          assert(v7[2] == "name3");
-          assert(v7[3] == "name4");
+          EXPECT_TRUE(v7.size() == 4);
+          EXPECT_TRUE(v7[0] == "name1");
+          EXPECT_TRUE(v7[1] == "name2");
+          EXPECT_TRUE(v7[2] == "name3");
+          EXPECT_TRUE(v7[3] == "name4");
           std::vector<std::string> v8 = read_name_list(ss8);
-          assert(v8.size() == 4);
-          assert(v8[0] == "name1");
-          assert(v8[1] == "name2");
-          assert(v8[2] == "name3");
-          assert(v8[3] == "name4");
+          EXPECT_TRUE(v8.size() == 4);
+          EXPECT_TRUE(v8[0] == "name1");
+          EXPECT_TRUE(v8[1] == "name2");
+          EXPECT_TRUE(v8[2] == "name3");
+          EXPECT_TRUE(v8[3] == "name4");
 
           // (I skip ss9 for easy of typing)
 
@@ -341,10 +339,10 @@ namespace wali {
           std::stringstream ss12("(a,b, c), (d, e, f)");
           std::stringstream ss13("(a,b, c), (d, e, f), (g, h, i) }");
 
-          assert(read_triple_list(ss10).size() == 1);
-          assert(read_triple_list(ss11).size() == 1);
-          assert(read_triple_list(ss12).size() == 2);
-          assert(read_triple_list(ss13).size() == 3);
+          EXPECT_TRUE(read_triple_list(ss10).size() == 1);
+          EXPECT_TRUE(read_triple_list(ss11).size() == 1);
+          EXPECT_TRUE(read_triple_list(ss12).size() == 2);
+          EXPECT_TRUE(read_triple_list(ss13).size() == 3);
     
           // Test read_quad_list. Again fairly light on purpose
           std::stringstream ss14("(a,b, c,d)");
@@ -352,10 +350,10 @@ namespace wali {
           std::stringstream ss16("(a,b,c,d),(d,e,f,g), (oth, 97go, n.hy429,<0932g>)");
           std::stringstream ss17("(a,b, c,g), (d, e, f,gc) }");
 
-          assert(read_quad_list(ss14).size() == 1);
-          assert(read_quad_list(ss15).size() == 1);
-          assert(read_quad_list(ss16).size() == 3);
-          assert(read_quad_list(ss17).size() == 2);
+          EXPECT_TRUE(read_quad_list(ss14).size() == 1);
+          EXPECT_TRUE(read_quad_list(ss15).size() == 1);
+          EXPECT_TRUE(read_quad_list(ss16).size() == 3);
+          EXPECT_TRUE(read_quad_list(ss17).size() == 2);
         }
 
 
@@ -376,13 +374,12 @@ namespace wali {
         
 
 
-        void
-        test_read_blocks()
+        TEST(wali$nwa$parser$$readBlocks, unitTests)
         {
           NWARefPtr nwa = new wali::nwa::NWA();
     
           // These tests call read_*_block (implicitly making sure there are no
-          // exceptions or assertions), then make sure they read everything they
+          // exceptions or EXPECT_TRUEions), then make sure they read everything they
           // should. For the first few tests, that means reading to the
           // end-of-string; later it means reading until the Z.
           std::stringstream ss1("Q: { }");
@@ -397,11 +394,11 @@ namespace wali {
           read_state_block(ss4, nwa);
           read_state_block(ss5, nwa);
 
-          assert(ss1.peek() == -1);
-          assert(ss2.peek() == -1);
-          assert(ss3.peek() == -1);
-          assert(ss4.peek() == -1);
-          assert(ss5.peek() == -1);
+          EXPECT_TRUE(ss1.peek() == -1);
+          EXPECT_TRUE(ss2.peek() == -1);
+          EXPECT_TRUE(ss3.peek() == -1);
+          EXPECT_TRUE(ss4.peek() == -1);
+          EXPECT_TRUE(ss5.peek() == -1);
     
           std::stringstream ss6("Q: a, b Z");
           std::stringstream ss7("Qf: a, b Z");
@@ -413,10 +410,10 @@ namespace wali {
           read_state_block(ss8, nwa);
           read_sigma_block(ss9, nwa);
 
-          assert(ss6.peek() == 'Z');
-          assert(ss7.peek() == 'Z');
-          assert(ss8.peek() == 'Z');
-          assert(ss9.peek() == 'Z');
+          EXPECT_TRUE(ss6.peek() == 'Z');
+          EXPECT_TRUE(ss7.peek() == 'Z');
+          EXPECT_TRUE(ss8.peek() == 'Z');
+          EXPECT_TRUE(ss9.peek() == 'Z');
 
           std::stringstream ss10("Delta_i: (a, b, c), (a, b, c) Z");
           std::stringstream ss11("Delta_c: (a, b, c), (a, b, c) Z");
@@ -426,15 +423,18 @@ namespace wali {
           read_delta_block(ss11, nwa);
           read_delta_block(ss12, nwa);
 
-          assert(ss10.peek() == 'Z');
-          assert(ss11.peek() == 'Z');
-          assert(ss12.peek() == 'Z');
+          EXPECT_TRUE(ss10.peek() == 'Z');
+          EXPECT_TRUE(ss11.peek() == 'Z');
+          EXPECT_TRUE(ss12.peek() == 'Z');
         }
 
 
 
+      }
+    }
+  }
+}
 
-      }}}}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// READ_NWA
@@ -449,8 +449,7 @@ namespace wali {
   namespace nwa {
     using namespace parser::details;
 
-    void
-    test_read_map()
+    TEST(wali$nwa$parser$$readMap, unitTests)
     {
       std::string nwas =
         "nwa main : { Q0: start_m Qf: end_m }\n"
@@ -463,17 +462,16 @@ namespace wali {
 
       ProcedureMap m = read_nwa_proc_set(ss);
 
-      assert(m.size() == 4);
+      EXPECT_TRUE(m.size() == 4);
 
-      assert(m["main"]->sizeStates() == 2);
-      assert(m["foo"]->sizeStates() == 3);
-      assert(m["bar"]->sizeStates() == 4);
-      assert(m["baz"]->sizeStates() == 5);
+      EXPECT_TRUE(m["main"]->sizeStates() == 2);
+      EXPECT_TRUE(m["foo"]->sizeStates() == 3);
+      EXPECT_TRUE(m["bar"]->sizeStates() == 4);
+      EXPECT_TRUE(m["baz"]->sizeStates() == 5);
     }
 
-        
-    void
-    test_read_nwa()
+
+    TEST(wali$nwa$parser$$readNwa, unitTests)
     {
       std::string mattsNwa =
         "Q: {" "\n"
@@ -530,9 +528,9 @@ namespace wali {
       read_nwa(ss3);
       read_nwa(ss3);
 
-      assert(ss1.peek() == -1);
-      assert(ss2.peek() == -1);
-      assert(ss3.peek() == -1);
+      EXPECT_TRUE(ss1.peek() == -1);
+      EXPECT_TRUE(ss2.peek() == -1);
+      EXPECT_TRUE(ss3.peek() == -1);
 
     
       std::string duplicates =
@@ -545,7 +543,7 @@ namespace wali {
 
       std::stringstream ss4(duplicates);
       read_nwa(ss4);
-      assert(ss4.peek() == -1);
+      EXPECT_TRUE(ss4.peek() == -1);
     }
     
   } // namespace nwa
