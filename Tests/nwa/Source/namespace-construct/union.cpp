4903ad5c6cd76b11ef4b136b7b421e71f2a131df
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-27 17:03:27 +0000

    Added expected answers for union tests. Right now it asserts because the states overlap.
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1351 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/namespace-construct/union.cpp
+++ Tests/nwa/Source/namespace-construct/union.cpp
@@ -4,11 +4,15 @@
 #include "wali/nwa/query/language.hpp"
 #include "wali/nwa/construct/union.hpp"
 #include "wali/nwa/query/automaton.hpp"
+#include "wali/nwa/query/language.hpp"
 
 #include "Tests/nwa/Source/fixtures.hpp"
 #include "Tests/nwa/Source/int-client-info.hpp"
 #include "Tests/nwa/Source/class-NWA/supporting.hpp"
 
+using namespace wali::nwa;
+
+#define NUM_ELEMENTS(array)  (sizeof(array)/sizeof((array)[0]))
 
 static const NWA empty,
     balanced = AcceptsBalancedOnly().nwa,
@@ -18,8 +22,6 @@ static const NWA empty,
     maybe_right = AcceptsPossiblyUnbalancedRight().nwa,
     maybe_full = AcceptsPositionallyConsistentString().nwa;
     
-
-
 
 // WARNING: the order of these words must be consistent with the row & column
 //          order in the table 'expected_answers' below.
@@ -37,24 +39,46 @@ static const unsigned num_nwas = NUM_ELEMENTS(nwas);
 
 
 // WARNING: the order of the rows and columns in this table must be
-//          consistent with the order of 'words' and 'nwas' above.
+//          consistent with the order of 'nwas' above.
 //
-// "Is the row NWA a subset(eq) of the column NWA?"
-static bool expected_answers[][num_nwas] = {
-    /*                    empty      balanced    strict left    maybe left   strict right  maybe right   maybe full */
-    /* empty        */  { true,      true,       true,          true,        true,         true,         true },
-    /* balanced     */  { false,     true,       false,         true,        false,        true,         true },
-    /* strict left  */  { false,     false,      true,          true,        false,        false,        true },
-    /* maybe left   */  { false,     false,      false,         true,        false,        false,        true },
-    /* strict right */  { false,     false,      false,         false,       true,         true,         true },
-    /* maybe right  */  { false,     false,      false,         false,       false,        true,         true },
-    /* maybe full   */  { false,     false,      false,         false,       false,        false,        true }
+// "What is the union of the row and column?"
+static const NWA * const expected_answers[][num_nwas] = {
+    /*                    empty          balanced      strict left   maybe left   strict right   maybe right   maybe full */
+    /* empty        */  { &empty,        &balanced,    &strict_left, &maybe_left, &strict_right, &maybe_right, &maybe_full },
+    /* balanced     */  { &balanced,     &balanced,    &maybe_left,  &maybe_left, &maybe_right,  &maybe_right, &maybe_full },
+    /* strict left  */  { &strict_left,  &maybe_left,  &strict_left, &maybe_left, NULL,          &maybe_full,  &maybe_full },
+    /* maybe left   */  { &maybe_left,   &maybe_left,  &maybe_left,  &maybe_left, &maybe_full,   &maybe_full,  &maybe_full },
+    /* strict right */  { &strict_right, &maybe_right, NULL,         &maybe_full, &strict_right, &maybe_right, &maybe_full },
+    /* maybe right  */  { &maybe_right,  &maybe_right, &maybe_full,  &maybe_full, &maybe_right,  &maybe_right, &maybe_full },
+    /* maybe full   */  { &maybe_full,   &maybe_full,  &maybe_full,  &maybe_full, &maybe_full,   &maybe_full,  &maybe_full }
 };
 
 
 namespace wali {
     namespace nwa {
         namespace construct {
+
+            TEST(wali$nwa$construct$$union, testBatteryOfVariouslyBalancedNwas)
+            {
+                for (unsigned left = 0 ; left < num_nwas ; ++left) {
+                    for (unsigned right = 0 ; right < num_nwas ; ++right) {
+                        std::stringstream ss;
+                        ss << "Testing NWA " << left << " union " << right;
+                        SCOPED_TRACE(ss.str());
+
+                        NWA const * expected_answer = expected_answers[left][right];
+
+                        // First, make sure that we expect an answer
+                        // (i.e. it's not no_answer).
+                        if (expected_answer) {
+                            NWARefPtr u = unionNWA(nwas[left], nwas[right]);
+                            
+                            EXPECT_TRUE(query::languageEquals(*expected_answer, *u));
+                        }
+                    } // for right
+                } // for left
+            }
+            
 
             TEST(wali$nwa$construct$$union, resultingAutomatonIsNondeterministic)
             {
