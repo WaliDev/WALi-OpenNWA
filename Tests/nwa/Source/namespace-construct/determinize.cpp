e2ecdb30f861f1da6dc952ea6c020ddad951ea09
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-27 22:59:05 +0000

    There's that broken test case.
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1369 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/namespace-construct/determinize.cpp
+++ Tests/nwa/Source/namespace-construct/determinize.cpp
@@ -14,6 +14,26 @@ namespace wali {
     namespace nwa {
         namespace construct {
 
+            TEST(wali$nwa$construct$$determinize, takeEpsilonClosureAroundCall)
+            {
+                SomeElements e;
+                NWA nwa;
+
+                //              *          symbol   ______
+                //  --> state -----> state2 ----> ((state3))
+                //                                  ``````
+                
+                nwa.addInitialState(e.state);
+                nwa.addFinalState(e.state3);
+
+                nwa.addInternalTrans(e.state, WALI_EPSILON, e.state2);
+                nwa.addCallTrans(e.state2, e.symbol, e.state3);
+
+                NWARefPtr det = determinize(nwa);
+
+                EXPECT_FALSE(query::languageIsEmpty(*det));
+            }
+            
         }
     }
 }
