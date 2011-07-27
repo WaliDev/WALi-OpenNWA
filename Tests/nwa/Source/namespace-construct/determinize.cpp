0d048124978b711c28f3deb8717179348e487c6c
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-27 22:51:09 +0000

    Going to add a determinize test because it's brokie
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1368 dfa36f5b-b2fa-0310-9605-e5aca7e64462

---
+++ Tests/nwa/Source/namespace-construct/determinize.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+#include "wali/nwa/NWA.hpp"
+#include "wali/nwa/query/language.hpp"
+#include "wali/nwa/construct/concatenate.hpp"
+#include "wali/nwa/query/automaton.hpp"
+#include "wali/nwa/query/language.hpp"
+
+#include "Tests/nwa/Source/fixtures.hpp"
+#include "Tests/nwa/Source/int-client-info.hpp"
+#include "Tests/nwa/Source/class-NWA/supporting.hpp"
+
+namespace wali {
+    namespace nwa {
+        namespace construct {
+
+        }
+    }
+}
+
