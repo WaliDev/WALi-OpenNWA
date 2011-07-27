2346cd295d3c2af8aae71dc919389856febee71a
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-27 16:44:13 +0000

    Skeleton for test battery from other file
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1350 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/namespace-construct/union.cpp
+++ Tests/nwa/Source/namespace-construct/union.cpp
@@ -9,6 +9,49 @@
 #include "Tests/nwa/Source/int-client-info.hpp"
 #include "Tests/nwa/Source/class-NWA/supporting.hpp"
 
+
+static const NWA empty,
+    balanced = AcceptsBalancedOnly().nwa,
+    strict_left = AcceptsStrictlyUnbalancedLeft().nwa,
+    maybe_left = AcceptsPossiblyUnbalancedLeft().nwa,
+    strict_right = AcceptsStrictlyUnbalancedRight().nwa,
+    maybe_right = AcceptsPossiblyUnbalancedRight().nwa,
+    maybe_full = AcceptsPositionallyConsistentString().nwa;
+    
+
+
+
+// WARNING: the order of these words must be consistent with the row & column
+//          order in the table 'expected_answers' below.
+static NWA const nwas[] = {
+    empty,
+    balanced,
+    strict_left,
+    maybe_left,
+    strict_right,
+    maybe_right,
+    maybe_full
+};
+
+static const unsigned num_nwas = NUM_ELEMENTS(nwas);
+
+
+// WARNING: the order of the rows and columns in this table must be
+//          consistent with the order of 'words' and 'nwas' above.
+//
+// "Is the row NWA a subset(eq) of the column NWA?"
+static bool expected_answers[][num_nwas] = {
+    /*                    empty      balanced    strict left    maybe left   strict right  maybe right   maybe full */
+    /* empty        */  { true,      true,       true,          true,        true,         true,         true },
+    /* balanced     */  { false,     true,       false,         true,        false,        true,         true },
+    /* strict left  */  { false,     false,      true,          true,        false,        false,        true },
+    /* maybe left   */  { false,     false,      false,         true,        false,        false,        true },
+    /* strict right */  { false,     false,      false,         false,       true,         true,         true },
+    /* maybe right  */  { false,     false,      false,         false,       false,        true,         true },
+    /* maybe full   */  { false,     false,      false,         false,       false,        false,        true }
+};
+
+
 namespace wali {
     namespace nwa {
         namespace construct {
