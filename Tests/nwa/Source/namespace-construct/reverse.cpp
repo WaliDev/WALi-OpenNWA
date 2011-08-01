398024a5b31d72f09c1cc7a39a37a6d92b4febc3
Evan Driscoll <driscoll@cs.wisc.edu>
2011-08-01 15:53:39 +0000

    Added (non-passing) tests of reverse
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1389 dfa36f5b-b2fa-0310-9605-e5aca7e64462

---
+++ Tests/nwa/Source/namespace-construct/reverse.cpp
@@ -0,0 +1,76 @@
+#include "gtest/gtest.h"
+
+#include "wali/nwa/NWA.hpp"
+#include "wali/nwa/query/language.hpp"
+#include "wali/nwa/construct/reverse.hpp"
+
+#include "Tests/nwa/Source/fixtures.hpp"
+#include "Tests/nwa/Source/class-NWA/supporting.hpp"
+
+
+using namespace wali::nwa;
+
+#define NUM_ELEMENTS(array)  (sizeof(array)/sizeof((array)[0]))
+
+// For every case, L(nwa[i])* = L(nwa[i]), except that the strictly
+// unbalanced ones don't accept epsilon.
+static NWA const nwas[] = {
+    NWA(),
+    AcceptsBalancedOnly().nwa,
+    AcceptsStrictlyUnbalancedLeft().nwa,
+    AcceptsPossiblyUnbalancedLeft().nwa,
+    AcceptsStrictlyUnbalancedRight().nwa,
+    AcceptsPossiblyUnbalancedRight().nwa,
+    AcceptsPositionallyConsistentString().nwa
+};
+
+static const unsigned num_nwas = NUM_ELEMENTS(nwas);
+
+
+static NWA const expected_answers[] = {
+    NWA(),
+    AcceptsBalancedOnly().nwa,
+    AcceptsStrictlyUnbalancedRight().nwa,
+    AcceptsPossiblyUnbalancedRight().nwa,
+    AcceptsStrictlyUnbalancedLeft().nwa,
+    AcceptsPossiblyUnbalancedLeft().nwa,
+    AcceptsPositionallyConsistentString().nwa
+};
+
+
+namespace wali {
+    namespace nwa {
+        namespace construct {
+
+            TEST(wali$nwa$query$$reverse, testBatteryOfVariouslyBalancedNwas)
+            {
+                for (unsigned nwa = 0 ; nwa < num_nwas ; ++nwa) {
+                    std::stringstream ss;
+                    ss << "Testing NWA " << nwa;
+                    SCOPED_TRACE(ss.str());
+
+                    NWARefPtr r = reverse(nwas[nwa]);
+
+                    EXPECT_TRUE(query::languageEquals(expected_answers[nwa], *r));
+                }
+            }
+
+
+            TEST(wali$nwa$query$$reverse, universalNwa)
+            {
+                NWA nwa;
+                SomeElements e;
+
+                nwa.addInitialState(e.state);
+                nwa.addFinalState(e.state);
+                nwa.addInternalTrans(e.state, e.symbol, e.state);
+                nwa.addCallTrans(e.state, e.symbol, e.state);
+                nwa.addReturnTrans(e.state, e.state, e.symbol, e.state);
+
+                NWARefPtr closure = star(nwa);
+
+                EXPECT_TRUE(query::languageEquals(nwa, *closure));
+            }
+        }
+    }
+}
