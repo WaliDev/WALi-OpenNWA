fd0eed47f51331fe1479cc42373fae7716c47d2b
Evan Driscoll <driscoll@cs.wisc.edu>
2011-08-22 22:32:23 +0000

    Change #include to match last commit
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1455 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/namespace-nwa_pds/nwa-to-wpds.cpp
+++ Tests/nwa/Source/namespace-nwa_pds/nwa-to-wpds.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 
 #include "wali/nwa/NWA.hpp"
-#include "wali/nwa/nwa_pds/NWAToPDS.hpp"
+#include "wali/nwa/nwa_pds/conversions.hpp"
 #include "wali/Reach.hpp"
 
 #include "Tests/nwa/Source/fixtures.hpp"
