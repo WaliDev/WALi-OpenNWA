0b5d7edfc7c8e00af40f36f04ee4df14f4202d87
Evan Driscoll <driscoll@cs.wisc.edu>
2011-08-22 20:55:03 +0000

    Added diff-based tests for NWA to WPDS functions
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1450 dfa36f5b-b2fa-0310-9605-e5aca7e64462

---
+++ Tests/nwa/Source/namespace-nwa_pds/answers-nwa-to-pds-returns.cpp
@@ -0,0 +1,89 @@
+#include <string>
+
+std::string answers_forwards_returns[] = {
+    //NWA 0
+    ""
+    ,
+
+    //NWA 1
+    "<(program,(q1b,(q0b,q0b))), q0b> -> <program, q0b>\tONE\n"
+    "<program, q0b> -> <program, q0b>\tONE\n"
+    "<program, q0b> -> <program, q1b q0b>\tONE\n"
+    "<(program,(q1b,(q1b,q1b))), q1b> -> <program, q1b>\tONE\n"
+    "<program, q1b> -> <program, q1b>\tONE\n"
+    "<program, q1b> -> <program, q1b q1b>\tONE\n"
+    "<program, q1b> -> <(program,(q1b,(q0b,q0b))), >\tONE\n"
+    "<program, q1b> -> <(program,(q1b,(q1b,q1b))), >\tONE\n"
+    ,
+
+    //NWA 2
+    "<(program,(q0sr,(q0sr,q2sr))), q2sr> -> <program, q2sr>\tONE\n"
+    "<program, q2sr> -> <program, q2sr>\tONE\n"
+    "<program, q2sr> -> <program, q3sr q2sr>\tONE\n"
+    "<program, q2sr> -> <(program,(q2sr,(q0sr,q2sr))), >\tONE\n"
+    "<(program,(q1sr,(q0sr,q0sr))), q0sr> -> <program, q0sr>\tONE\n"
+    "<program, q1sr> -> <program, q1sr>\tONE\n"
+    "<program, q1sr> -> <program, q1sr q1sr>\tONE\n"
+    "<program, q1sr> -> <(program,(q1sr,(q0sr,q0sr))), >\tONE\n"
+    "<program, q1sr> -> <(program,(q1sr,(q1sr,q1sr))), >\tONE\n"
+    "<(program,(q3sr,(q2sr,q2sr))), q2sr> -> <program, q2sr>\tONE\n"
+    "<(program,(q1sr,(q1sr,q1sr))), q1sr> -> <program, q1sr>\tONE\n"
+    "<(program,(q3sr,(q3sr,q3sr))), q3sr> -> <program, q3sr>\tONE\n"
+    "<(program,(q2sr,(q0sr,q2sr))), q2sr> -> <program, q2sr>\tONE\n"
+    "<program, q3sr> -> <program, q3sr>\tONE\n"
+    "<program, q3sr> -> <program, q3sr q3sr>\tONE\n"
+    "<program, q3sr> -> <(program,(q3sr,(q2sr,q2sr))), >\tONE\n"
+    "<program, q3sr> -> <(program,(q3sr,(q3sr,q3sr))), >\tONE\n"
+    "<program, q0sr> -> <program, q0sr>\tONE\n"
+    "<program, q0sr> -> <program, q1sr q2sr>\tONE\n"
+    "<program, q0sr> -> <program, q1sr q0sr>\tONE\n"
+    "<program, q0sr> -> <(program,(q0sr,(q0sr,q2sr))), >\tONE\n"
+    ,
+
+    //NWA 3
+    "<(program,(q0mr,(q0mr,q0mr))), q0mr> -> <program, q0mr>\tONE"  "\n"
+    "<program, q1mr> -> <program, q1mr>\tONE"  "\n"
+    "<program, q1mr> -> <program, q1mr q1mr>\tONE"  "\n"
+    "<program, q1mr> -> <(program,(q1mr,(q0mr,q0mr))), >\tONE"  "\n"
+    "<program, q1mr> -> <(program,(q1mr,(q1mr,q1mr))), >\tONE"  "\n"
+    "<program, q0mr> -> <program, q0mr>\tONE"  "\n"
+    "<program, q0mr> -> <program, q1mr q0mr>\tONE"  "\n"
+    "<program, q0mr> -> <(program,(q0mr,(q0mr,q0mr))), >\tONE"  "\n"
+    "<(program,(q1mr,(q1mr,q1mr))), q1mr> -> <program, q1mr>\tONE"  "\n"
+    "<(program,(q1mr,(q0mr,q0mr))), q0mr> -> <program, q0mr>\tONE"  "\n"
+    ,
+
+    //NWA 4
+    "<(program,(q1sl,(q1sl,q1sl))), q1sl> -> <program, q1sl>\tONE"  "\n"
+    "<program, q1sl> -> <program, q1sl>\tONE"  "\n"
+    "<program, q1sl> -> <(program,(q1sl,(q0sl,q0sl))), >\tONE"  "\n"
+    "<program, q1sl> -> <(program,(q1sl,(q1sl,q1sl))), >\tONE"  "\n"
+    "<program, q1sl> -> <program, q1sl q1sl>\tONE"  "\n"
+    "<(program,(q1sl,(q0sl,q0sl))), q0sl> -> <program, q0sl>\tONE"  "\n"
+    "<program, q0sl> -> <program, q0sl>\tONE"  "\n"
+    "<program, q0sl> -> <program, q1sl q0sl>\tONE"  "\n"
+    ,
+
+    //NWA 5
+    "<program, q1ml> -> <program, q1ml>\tONE"  "\n"
+    "<program, q1ml> -> <(program,(q1ml,(q0ml,q0ml))), >\tONE"  "\n"
+    "<program, q1ml> -> <(program,(q1ml,(q1ml,q1ml))), >\tONE"  "\n"
+    "<program, q1ml> -> <program, q1ml q1ml>\tONE"  "\n"
+    "<(program,(q1ml,(q0ml,q0ml))), q0ml> -> <program, q0ml>\tONE"  "\n"
+    "<(program,(q1ml,(q1ml,q1ml))), q1ml> -> <program, q1ml>\tONE"  "\n"
+    "<program, q0ml> -> <program, q0ml>\tONE"  "\n"
+    "<program, q0ml> -> <program, q1ml q0ml>\tONE"  "\n"
+    ,
+
+    //NWA 6
+    "<program, q0mf> -> <program, q0mf>\tONE"  "\n"
+    "<program, q0mf> -> <program, q1mf q0mf>\tONE"  "\n"
+    "<program, q0mf> -> <(program,(q0mf,(q0mf,q0mf))), >\tONE"  "\n"
+    "<(program,(q1mf,(q1mf,q1mf))), q1mf> -> <program, q1mf>\tONE"  "\n"
+    "<(program,(q1mf,(q0mf,q0mf))), q0mf> -> <program, q0mf>\tONE"  "\n"
+    "<(program,(q0mf,(q0mf,q0mf))), q0mf> -> <program, q0mf>\tONE"  "\n"
+    "<program, q1mf> -> <program, q1mf>\tONE"  "\n"
+    "<program, q1mf> -> <program, q1mf q1mf>\tONE"  "\n"
+    "<program, q1mf> -> <(program,(q1mf,(q0mf,q0mf))), >\tONE"  "\n"
+    "<program, q1mf> -> <(program,(q1mf,(q1mf,q1mf))), >\tONE"  "\n"
+};
---
+++ Tests/nwa/Source/namespace-nwa_pds/nwa-to-wpds.cpp
@@ -0,0 +1,107 @@
+#include "gtest/gtest.h"
+
+#include "wali/nwa/NWA.hpp"
+#include "wali/nwa/nwa_pds/NWAToPDS.hpp"
+#include "wali/Reach.hpp"
+
+#include "Tests/nwa/Source/fixtures.hpp"
+
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace wali::nwa;
+using namespace wali::wpds;
+using namespace std;
+using boost::algorithm::split;
+using boost::algorithm::is_any_of;
+
+#define NUM_ELEMENTS(array)  (sizeof(array)/sizeof((array)[0]))
+
+// WARNING: the order of these words must be consistent with the row & column
+//          order in the table 'expected_answers' below.
+static NWA const nwas[] = {
+    NWA(),
+    AcceptsBalancedOnly().nwa,
+    AcceptsStrictlyUnbalancedLeft().nwa,
+    AcceptsPossiblyUnbalancedLeft().nwa,
+    AcceptsStrictlyUnbalancedRight().nwa,
+    AcceptsPossiblyUnbalancedRight().nwa,
+    AcceptsPositionallyConsistentString().nwa
+};
+
+static const unsigned num_nwas = NUM_ELEMENTS(nwas);
+
+
+extern std::string answers_forwards_returns[],
+    answers_forwards_calls[],
+    answers_backwards_returns[],
+    answers_backwards_calls[];
+
+
+
+
+namespace wali {
+    namespace nwa {
+        namespace nwa_pds {
+
+            template<typename Container>
+            void sort(Container & c)
+            {
+                std::sort(c.begin(), c.end());
+            }
+
+            TEST(wali$nwa$nwa_pds$$NwaToWpdsCalls, testBatteryOfVariouslyBalancedNwas)
+            {
+                for (unsigned nwa = 0 ; nwa < num_nwas ; ++nwa) {
+                    std::stringstream ss;
+                    ss << "Testing NWA " << nwa;
+                    SCOPED_TRACE(ss.str());
+
+                    // {(f)orward, (b)ackward} x {(c)all, (r)eturn}
+                    string fc = NwaToWpdsCalls(nwas[nwa], ReachGen()).toString();
+                    string fr = NwaToWpdsReturns(nwas[nwa], ReachGen()).toString();
+                    string bc = NwaToBackwardsWpdsCalls(nwas[nwa], ReachGen()).toString();
+                    string br = NwaToBackwardsWpdsReturns(nwas[nwa], ReachGen()).toString();
+
+                    // Each is a string. We need to split it into lines, then
+                    // sort the lines, as the rules can be printed in random
+                    // order.
+
+                    vector<string>
+                        actual_fc, actual_fr, actual_bc, actual_br,
+                        expected_fc, expected_fr, expected_bc, expected_br;
+                    
+                    split(actual_fc, fc, is_any_of("\n"));
+                    split(actual_fr, fr, is_any_of("\n"));
+                    split(actual_bc, bc, is_any_of("\n"));
+                    split(actual_br, br, is_any_of("\n"));
+
+                    split(expected_fc, answers_forwards_calls[nwa], is_any_of("\n"));
+                    split(expected_fr, answers_forwards_returns[nwa], is_any_of("\n"));
+                    split(expected_bc, answers_backwards_calls[nwa], is_any_of("\n"));
+                    split(expected_br, answers_backwards_returns[nwa], is_any_of("\n"));
+
+                    sort(actual_fc);
+                    sort(actual_fr);
+                    sort(actual_bc);
+                    sort(actual_br);
+
+                    sort(expected_fc);
+                    sort(expected_fr);
+                    sort(expected_bc);
+                    sort(expected_br);
+                    
+                    EXPECT_EQ(expected_fc, actual_fc);
+                    EXPECT_EQ(expected_fr, actual_fr);
+                    EXPECT_EQ(expected_bc, actual_bc);
+                    EXPECT_EQ(expected_br, actual_br);
+                }
+            }
+            
+        }
+    }
+}
