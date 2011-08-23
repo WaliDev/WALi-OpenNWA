65fd68dacae2c623b0bc531fc3449484a3778248
Evan Driscoll <driscoll@cs.wisc.edu>
2011-08-23 18:33:41 +0000

    Added a test to exhibit a determinize bug
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1461 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Tests/nwa/Source/namespace-construct/determinize.cpp
+++ Tests/nwa/Source/namespace-construct/determinize.cpp
@@ -53,7 +53,60 @@ namespace wali {
                 EXPECT_FALSE(query::languageIsEmpty(*det));
             }
 
-            
+
+            TEST(wali$nwa$construct$$determinize, useADifferentInitialStateToStartAndAsAPendingReturn)
+            {
+                SomeElements e;
+                NWA nwa;
+
+                // --> state
+                // --> state2 -----------------------> ((state3))
+                //            symbol/state (as return)
+
+                nwa.addInitialState(e.state);
+                nwa.addInitialState(e.state2);
+                nwa.addFinalState(e.state3);
+
+                nwa.addReturnTrans(e.state2, e.state, e.symbol, e.state3);
+
+                NWARefPtr det = determinize(nwa);
+
+                // Now test to see if it's got 'symbol(' in it, and that it
+                // doesn't contain epsilon
+                NestedWord empty, word;
+                word.appendReturn(e.symbol);
+
+                EXPECT_FALSE(query::languageContains(*det, empty));
+                EXPECT_TRUE(query::languageContains(*det, word));
+
+                // Some structure tests. We'll remove the stuck state, then
+                // make sure it's isomorphic to
+                //       --> o ------> (o)
+                // as best we can.
+                //
+                // I *think* that if all these tests pass, combined with the
+                // language tests below, there is only one NWA that can be,
+                // and it's correct. (This may not be the case if {} had
+                // outgoing transitions though.)
+                EXPECT_EQ(3u, det->sizeStates());
+
+                State stuck = getKey("{}"); // implementation detail
+                ASSERT_TRUE(det->isState(stuck));
+                EXPECT_TRUE(!det->isFinalState(stuck));
+
+                det->removeState(stuck);
+
+                EXPECT_EQ(2u, det->sizeStates());
+                EXPECT_EQ(1u, det->sizeInitialStates());
+                EXPECT_EQ(1u, det->sizeFinalStates());
+                EXPECT_EQ(1u, det->sizeSymbols());
+                EXPECT_EQ(0u, det->sizeInternalTrans());
+                EXPECT_EQ(0u, det->sizeCallTrans());
+                EXPECT_EQ(1u, det->sizeReturnTrans());
+
+                EXPECT_FALSE(query::languageContains(*det, empty));
+                EXPECT_TRUE(query::languageContains(*det, word));
+            }
         }
     }
 }
