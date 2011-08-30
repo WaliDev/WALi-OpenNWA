5653eedf80accb760a38cd511d750578591adb99
Evan Driscoll <driscoll@cs.wisc.edu>
2011-08-30 16:03:48 +0000

    Remove unneeded includes
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1528 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Source/wali/nwa/NWA.hpp
+++ Source/wali/nwa/NWA.hpp
@@ -31,9 +31,6 @@
 #include "wali/nwa/construct/reverse.hpp"
 #include "wali/nwa/construct/star.hpp"
 #include "wali/nwa/construct/union.hpp"
-#include "wali/nwa/nwa_pds/conversions.hpp"
-#include "wali/nwa/nwa_pds/plusWpds.hpp"
-#include "wali/nwa/query/language.hpp"
 
 //#define USE_BUDDY
 #ifdef USE_BUDDY
