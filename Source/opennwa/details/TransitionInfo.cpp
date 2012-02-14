08948ddddc72e2307734c348946c8af9b3dbb5ad
Evan Driscoll <driscoll@cs.wisc.edu>
2012-02-14 20:49:55 +0000

    Avoid MSVC warning about empty object file
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1651 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Source/opennwa/details/TransitionInfo.cpp
+++ Source/opennwa/details/TransitionInfo.cpp
@@ -1,2 +1,9 @@
 #include "TransitionInfo.hpp"
 
+// Just here for compilation. MSVC warns about an empty object file, so...
+
+namespace opennwa {
+  namespace details {
+    int dummy_variable_to_avoid_msvc_warning;
+  }
+}
