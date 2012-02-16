f7b329ba2c73d22a7e00b5be3b448ee34f73e667
Evan Driscoll <driscoll@cs.wisc.edu>
2012-02-16 23:02:23 +0000

    Pull the switcheroo: use the multiset and default worklist implementation if Boost 1.49 isn't available, and use boost if it is.
    Of course, we don't really use boost yet, but that's next...
    
    
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1682 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Source/wali/PriorityWorklist.hpp
+++ Source/wali/PriorityWorklist.hpp
@@ -1,51 +1,58 @@
 #ifndef wali_PRIORITY_WORKLIST_GUARD
 #define wali_PRIORITY_WORKLIST_GUARD 1
 
-/*!
- * @author Nicholas Kidd
- */
+#include <boost/version.hpp>
+
+#if defined(BOOST_VERSION) && BOOST_VERSION >= 104900
+#    define WALI_HAS_BOOST_HEAP 1
+#else
+#    define WALI_HAS_BOOST_HEAP 0
+#endif
+
 
 #include "wali/Common.hpp"
 #include "wali/Worklist.hpp"
 #include "wali/wfa/Trans.hpp"
-#include <map>
-#include <set>
-#include <functional>
+
+
+
+#if WALI_HAS_BOOST_HEAP
+
+#    include "wali/details/PriorityWorklist.boost_heap.hpp"
 
 namespace wali
 {
   template<typename Compare>
-  class PriorityWorklist : public Worklist<wfa::ITrans>
-  {
-      // Boost implements max-heaps, not min-heaps. We want to hide this
-      // pecularity, so negate the comparison functor.
-      typedef std::binary_negate<Compare> NotCompare;
-      
-    public:
-      typedef std::multiset< wfa::ITrans*, NotCompare > pwl_t;
+  class PriorityWorklist : public wali::details::BoostHeapPriorityWorklist<Compare>
+  {};
+}
 
-    public:
-      PriorityWorklist();
+#else
+#    if HAS_PRAGMA_MESSAGE
+#        pragma message ("NOTE: Proper priority queues require Boost 1.49 or higher (with the Heap library). Disabling these and replacing with the default, unordered worklist.")
+#    endif
 
-      virtual ~PriorityWorklist();
+#    include "wali/DefaultWorklist.hpp"
 
-      virtual bool put( wfa::ITrans *t );
+namespace wali
+{
+  template<typename Compare>
+  class PriorityWorklist : public DefaultWorklist<wfa::ITrans>
+  {};
+}
 
-      virtual wfa::ITrans * get();
+#endif
 
-      virtual bool empty() const;
 
-      virtual void clear();
 
-    protected:
-      pwl_t workset;
+#include "wali/details/PriorityWorklist.multiset.hpp"
 
-  }; // class PriorityWorklist
-
-} // namespace wali
-
-#include "wali/PriorityWorklist.cpp"
-
+namespace wali
+{
+  template<typename Compare>
+  class FixedPriorityWorklist : public wali::details::MultisetPriorityWorklist<Compare>
+  {};
+}
 
 // Yo, Emacs!
 // Local Variables:
