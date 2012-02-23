6c3daa5bd60a5b9166ac92672c192480d583cf70
Evan Driscoll <driscoll@cs.wisc.edu>
2012-02-23 18:08:49 +0000

    Add in error checking for bad mbstowcs calls
    git-svn-id: file:///p/wpis/people/students/kidd/svnwali/trunk@1722 dfa36f5b-b2fa-0310-9605-e5aca7e64462

--- Source/wali/Common.cpp
+++ Source/wali/Common.cpp
@@ -102,6 +102,14 @@ namespace wali
     std::cerr << "In function " << function <<":\n";
     size_t sz_assertion = mbstowcs(NULL, assertion, 0);
     size_t sz_file = mbstowcs(NULL, file, 0);
+    if (sz_assertion == 0 || sz_file == 0) {
+        std::cerr << "  file: " << file << "\n"
+                  << "  line: " << line << "\n"
+                  << "  assertion failure!\n"
+                  << "\n"
+                  << "  There was also an error converting one of the above to a wchar_t*";
+        abort();
+    } 
     wchar_t* wassertion = new wchar_t[sz_assertion + 1];
     wchar_t* wfile = new wchar_t[sz_file + 1];
     mbstowcs(wassertion, assertion, sz_assertion);
