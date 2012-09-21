6b6f127dcb5475220eb2de22c342d550a6fa5870
Evan Driscoll <driscoll@cs.wisc.edu>
2012-09-21 11:18:49 -0500

    Add a couple sanity checks

--- AddOns/Xfa/Source/wali/xfa/Xfa.hpp
+++ AddOns/Xfa/Source/wali/xfa/Xfa.hpp
@@ -172,6 +172,8 @@ namespace cfglib {
                 const int block_size = 4096; // amt to read at once
 
                 while (true) {
+                    assert(data.size() < 2000000000u);
+
                     // Make room for another block
                     data.resize(data.size() + block_size);
 
@@ -179,6 +181,7 @@ namespace cfglib {
 
                     // Read into that new space
                     int size_just_read = std::fread(&data[data.size()-block_size], 1u, block_size, file);
+                    assert(size_just_read <= block_size);
 
                     if (size_just_read < block_size) {
                         // We hit EOF (or an error...)
