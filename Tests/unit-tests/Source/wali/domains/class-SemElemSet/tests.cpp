0d10037d3b6a1afc402fedf9c8793b5a255c6b9b
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-30 16:59:37 -0600

    Tests for SemElemSet's KeepMaximalElements/KeepMinimalElements

--- Tests/unit-tests/Source/wali/domains/class-SemElemSet/tests.cpp
+++ Tests/unit-tests/Source/wali/domains/class-SemElemSet/tests.cpp
@@ -32,6 +32,28 @@ namespace wali {
             set.insert(element);
         }
 
+        void insert(SemElemSet::ElementSet & set, sem_elem_t e1, sem_elem_t e2)
+        {
+            insert(set, e1);
+            insert(set, e2);
+        }
+
+        void insert(SemElemSet::ElementSet & set, sem_elem_t e1, sem_elem_t e2, sem_elem_t e3)
+        {
+            insert(set, e1);
+            insert(set, e2);
+            insert(set, e3);
+        }
+
+        void insert(SemElemSet::ElementSet & set, sem_elem_t e1, sem_elem_t e2, sem_elem_t e3, sem_elem_t e4)
+        {
+            insert(set, e1);
+            insert(set, e2);
+            insert(set, e3);
+            insert(set, e4);
+        }
+
+
 
         TEST(wali$domains$SemElemSet$$SemElemSet, oneArgConstructorMakesZero)
         {
@@ -301,6 +323,155 @@ namespace wali {
 
             EXPECT_TRUE(s1.equal(&s2));
         }
+
+
+        TEST(wali$domains$SemElemSet, subsumedElementIsRemovedDuringCombine)
+        {
+            sem_elem_t
+                two = new ShortestPathSemiring(2),
+                ten = new ShortestPathSemiring(10);
+
+            SemElemSet::ElementSet two_set, ten_set;
+            insert(two_set, two);
+            insert(ten_set, ten);
+
+            SemElemSet
+                two_keeping_min(SemElemSet::KeepMinimalElements, two, two_set),
+                ten_keeping_min(SemElemSet::KeepMinimalElements, ten, ten_set),
+                two_keeping_max(SemElemSet::KeepMaximalElements, two, two_set),
+                ten_keeping_max(SemElemSet::KeepMaximalElements, ten, ten_set);
+
+            sem_elem_t
+                actual_min1 = two_keeping_min.combine(&ten_keeping_min),
+                actual_min2 = ten_keeping_min.combine(&two_keeping_min),
+                actual_max1 = two_keeping_max.combine(&ten_keeping_max),
+                actual_max2 = ten_keeping_max.combine(&two_keeping_max);
+
+            // Min/max are by the implicit lattice ordering, which is
+            // OPPOSITE of what you think because combine is min:
+            //    10 + 2 = 10 min 2 = 2, so 10 <= 2 in the lattice
+
+            ASSERT_TRUE(ten->underApproximates(two));            
+            
+            EXPECT_TRUE(ten_keeping_min.equal(actual_min1));
+            EXPECT_TRUE(ten_keeping_min.equal(actual_min2));
+
+            EXPECT_TRUE(two_keeping_max.equal(actual_max1));
+            EXPECT_TRUE(two_keeping_max.equal(actual_max2));
+        }
+
+
+        TEST(wali$domains$SemElemSet, setKeepsIncomparableElementsOnCreationAndCombine)
+        {
+            // Easiest way I can think of to test this is to use SemElemSets
+            // as elements in the SemElemSets that I'm actually interested
+            // in. (The simpler domains, like ShortestPath, are totally
+            // ordered.)
+
+            // So here we set up the ELEMENTS
+            sem_elem_t
+                two = new ShortestPathSemiring(2),
+                six = new ShortestPathSemiring(6);
+
+            SemElemSet::ElementSet empty_set, two_set, six_set, two_six_set;;
+            insert(two_set, two);
+            insert(six_set, six);
+            insert(two_six_set, two);
+            insert(two_six_set, six);
+
+            sem_elem_t
+                elem   = new SemElemSet(SemElemSet::KeepAllNonduplicates, two, empty_set),
+                elem2  = new SemElemSet(SemElemSet::KeepAllNonduplicates, two, two_set),
+                elem6  = new SemElemSet(SemElemSet::KeepAllNonduplicates, six, six_set),
+                elem26 = new SemElemSet(SemElemSet::KeepAllNonduplicates, six, two_six_set);
+
+            ASSERT_FALSE(elem2->underApproximates(elem6));
+            ASSERT_FALSE(elem6->underApproximates(elem2));
+            ASSERT_TRUE(elem->underApproximates(elem2));
+            ASSERT_TRUE(elem2->underApproximates(elem26));
+
+            // Here we set up the SETS
+
+            SemElemSet::ElementSet
+                set0, set1, set2, set3, set4, set5, set6, set7,
+                set8, set9, setA, setB, setC, setD, setE, setF;
+
+            //                                        //      Full set                    Keeping maximal            Keeping minimal
+            //                                        // {                     }        {          }   (set0)       {          } (set0)
+            insert(set1, elem                      ); // { <>                  }        { <>       }   (set1)       { <>       } (set1)
+            insert(set2,       elem2               ); // {     <2>             }        { <2>      }   (set2)       { <2>      } (set2)
+            insert(set3, elem, elem2               ); // { <>, <2>             }        { <2>      }   (set2*)      { <>       } (set1*)
+            insert(set4,              elem6        ); // {          <6>        }        { <6>      }   (set4)       { <6>      } (set4)
+            insert(set5, elem,        elem6        ); // { <>,      <6>        }        { <6>      }   (set4*)      { <>       } (set1*)
+            insert(set6,       elem2, elem6        ); // {     <2>, <6>        }        { <2>, <6> }   (set6)       { <2>, <6> } (set6)
+            insert(set7, elem, elem2, elem6        ); // { <>, <2>, <6>        }        { <2>, <6> }   (set6*)      { <>       } (set1*)
+            insert(set8,                     elem26); // {               <2,6> }        { <2,6>    }   (set8)       { <2, 6>   } (set8)
+            insert(set9, elem,               elem26); // { <>,           <2,6> }        { <2,6>    }   (set8*)      { <>       } (set1*)
+            insert(setA,       elem2,        elem26); // {     <2>,      <2,6> }        { <2,6>    }   (set8*)      { <2>      } (set2*)
+            insert(setB, elem, elem2,        elem26); // { <>, <2>,      <2,6> }        { <2,6>    }   (set8*)      { <>       } (set1*)
+            insert(setC,              elem6, elem26); // {          <6>, <2,6> }        { <2,6>    }   (set8*)      { <6>      } (set4*)
+            insert(setD, elem,        elem6, elem26); // { <>,      <6>, <2,6> }        { <2,6>    }   (set8*)      { <>       } (set1*)
+            insert(setE,       elem2, elem6, elem26); // {     <2>, <6>, <2,6> }        { <2,6>    }   (set8*)      { <2>, <6> } (set6*)
+            insert(setF, elem, elem2, elem6, elem26); // { <>, <2>, <6>, <2,6> }        { <2,6>    }   (set8*)      { <>       } (set1*)
+            
+
+#define SETS(root) \
+    root##_keeping_max(SemElemSet::KeepMaximalElements, elem->one(), root), \
+    root##_keeping_min(SemElemSet::KeepMinimalElements, elem->one(), root)
+            
+            SemElemSet
+                SETS(set0), SETS(set1), SETS(set2), SETS(set3), SETS(set4), SETS(set5), SETS(set6), SETS(set7),
+                SETS(set8), SETS(set9), SETS(setA), SETS(setB), SETS(setC), SETS(setD), SETS(setE), SETS(setF);
+
+#define EXPECT_SET_REDUCES_TO(root, maximal, minimal) \
+    EXPECT_TRUE(root##_keeping_min.equal(&minimal##_keeping_min)); \
+    EXPECT_TRUE(root##_keeping_max.equal(&maximal##_keeping_max))
+
+            EXPECT_SET_REDUCES_TO(set0, set0, set0);
+            EXPECT_SET_REDUCES_TO(set1, set1, set1);
+            EXPECT_SET_REDUCES_TO(set2, set2, set2);
+            EXPECT_SET_REDUCES_TO(set3, set2, set1);
+            EXPECT_SET_REDUCES_TO(set4, set4, set4);
+            EXPECT_SET_REDUCES_TO(set5, set4, set1);
+            EXPECT_SET_REDUCES_TO(set6, set6, set6);
+            EXPECT_SET_REDUCES_TO(set7, set6, set1);
+            EXPECT_SET_REDUCES_TO(set8, set8, set8);
+            EXPECT_SET_REDUCES_TO(set9, set8, set1);
+            EXPECT_SET_REDUCES_TO(setA, set8, set2);
+            EXPECT_SET_REDUCES_TO(setB, set8, set1);
+            EXPECT_SET_REDUCES_TO(setC, set8, set4);
+            EXPECT_SET_REDUCES_TO(setD, set8, set1);
+            EXPECT_SET_REDUCES_TO(setE, set8, set6);
+            EXPECT_SET_REDUCES_TO(setF, set8, set1);
+
+            sem_elem_t
+                should_be_set2_max1 = set1_keeping_max.combine(&set2_keeping_max),
+                should_be_set2_max2 = set2_keeping_max.combine(&set1_keeping_max),
+                should_be_set1_min1 = set1_keeping_min.combine(&set2_keeping_min),
+                should_be_set1_min2 = set2_keeping_min.combine(&set1_keeping_min),
+                should_be_set6_max1 = set2_keeping_max.combine(&set4_keeping_max),
+                should_be_set6_max2 = set4_keeping_max.combine(&set2_keeping_max),
+                should_be_set6_min1 = set2_keeping_min.combine(&set4_keeping_min),
+                should_be_set6_min2 = set4_keeping_min.combine(&set2_keeping_min),
+                should_be_set8_max1 = set8_keeping_max.combine(&set2_keeping_max),
+                should_be_set2_min1 = set8_keeping_min.combine(&set2_keeping_min);
+
+            EXPECT_TRUE(set2_keeping_max.equal(should_be_set2_max1));
+            EXPECT_TRUE(set2_keeping_max.equal(should_be_set2_max2));
+
+            EXPECT_TRUE(set1_keeping_min.equal(should_be_set1_min1));
+            EXPECT_TRUE(set1_keeping_min.equal(should_be_set1_min2));
+
+            EXPECT_TRUE(set6_keeping_max.equal(should_be_set6_max1));
+            EXPECT_TRUE(set6_keeping_max.equal(should_be_set6_max2));
+
+            EXPECT_TRUE(set6_keeping_min.equal(should_be_set6_min1));
+            EXPECT_TRUE(set6_keeping_min.equal(should_be_set6_min2));
+
+            EXPECT_TRUE(set8_keeping_max.equal(should_be_set8_max1));
+            EXPECT_TRUE(set2_keeping_min.equal(should_be_set2_min1));
+        }
+
         
     }
 }
