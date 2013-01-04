2661315116a7c7cb6d33b8c34ed9cf2fa783b8b6
Evan Driscoll <driscoll@cs.wisc.edu>
2013-01-04 13:47:35 -0600

    Starting testing DisjointSets:
    - Fix a couple compiler errors (private base class; missing consts)
    - Fix a problem where the cache was being replaced when it shouldn't
      have been, which meant that 'sets.begin() != sets.begin()' was
      true (as the first one was into one backing set, and then the second
      call replaced that cache with a new one and returned an iterator
      into it)

--- Tests/unit-tests/Source/AddOns/Xfa/wali/util/DisjointSets.cpp
+++ Tests/unit-tests/Source/AddOns/Xfa/wali/util/DisjointSets.cpp
@@ -2,3 +2,22 @@
 #include "gtest/gtest.h"
 #include "wali/util/DisjointSets.hpp"
 
+#include <string>
+
+namespace wali {
+    namespace util {
+
+        template class DisjointSets<std::string>;
+
+        typedef DisjointSets<std::string> StringSets;
+
+        TEST(wali$util$DisjointSets, emptyCollectionIsEmpty)
+        {
+            StringSets sets;
+
+            EXPECT_EQ(sets.begin(), sets.end());
+        }
+
+    }
+}
+
