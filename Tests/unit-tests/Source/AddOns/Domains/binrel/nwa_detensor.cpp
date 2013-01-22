9db175e44f50f8ba33459c2ef66e3784a3ecaa5d
Prathmesh <callpraths@gmail.com>
2013-01-21 19:00:04 -0600

    Some basic tests for detensorViaNwa

--- Tests/unit-tests/Source/AddOns/Domains/binrel/nwa_detensor.cpp
+++ Tests/unit-tests/Source/AddOns/Domains/binrel/nwa_detensor.cpp
@@ -27,32 +27,6 @@ using namespace wali::domains::binrel::details;
 
 namespace
 {
-  TEST(Nwa_detensor, printTest){
-    program_bdd_context_t brm = new ProgramBddContext();
-    map<string, int> vars;
-    vars["x1"] = 2;
-    vars["x2"] = 2;
-    brm->setIntVars(vars);
-
-    brm->setupLevelArray();
-    BddContext::VocLevelArray const & lvls = brm->getLevelArray();
-    for(BddContext::VocLevelArray::const_iterator cit = lvls.begin(); cit != lvls.end(); ++cit)
-      cout << *cit << " ";
-    cout << endl;
-    bdd a = brm->Assign("x1", (brm->From("x1"))) & brm->Assume(brm->From("x1"), brm->Const(0));
-    bdd b = brm->Assign("x2", (brm->From("x2"))) & brm->Assume(brm->From("x2"), brm->Const(0));
-    bdd c = a & b; 
-    binrel_t d = new BinRel(brm.get_ptr(), c); 
-    binrel_t e = dynamic_cast<BinRel*>(d->tensor(d.get_ptr()).get_ptr());
-   
-    bdd z = e->getBdd();
-
-    bdd_fnprintdot_levels("ttt.dot", z);
-
-    binrel_t y = new BinRel(brm.get_ptr(), z);
-    binrel_t x = dynamic_cast<BinRel*>(y->detensorTranspose().get_ptr());
-  }
-
   TEST(Nwa_detensor, paperExample){
     program_bdd_context_t brm = new ProgramBddContext();
     map<string, int> vars;
@@ -65,12 +39,82 @@ namespace
     binrel_t eye = dynamic_cast<BinRel*>(M->one().get_ptr());
     binrel_t s = dynamic_cast<BinRel*>(((M->tensor(M.get_ptr()))->combine(eye->tensor(eye.get_ptr()).get_ptr())).get_ptr());
     bdd z = s->getBdd();
-    bdd_fnprintdot_levels("mtmpiti.dot", z);
+    //bdd_fnprintdot_levels("mtmpiti.dot", z);
     binrel_t x = dynamic_cast<BinRel*>(s->detensorTranspose().get_ptr());
     z = x->getBdd();
-    bdd_fnprintdot_levels("detensored.dot", z);
+    ASSERT_TRUE(x->equal(x->one().get_ptr()));
+    //bdd_fnprintdot_levels("detensored.dot", z);
+
+    //b = brm->Assume(brm->From("x1"), brm->From("x1"));
+    //bdd_fnprintdot_levels("id.dot", b);
   }
 
+  TEST(Nwa_detensor, alltrue)
+  {
+    program_bdd_context_t brm = new ProgramBddContext();
+    map<string, int> vars;
+    vars["x1"] = 2;
+    vars["x2"] = 2;
+    vars["x3"] = 2;
+    brm->setIntVars(vars);
+    sem_elem_tensor_t b1 = new BinRel(brm.get_ptr(), bddtrue);
+    sem_elem_tensor_t b2 = new BinRel(brm.get_ptr(), bddtrue);
+    sem_elem_tensor_t b3 = b2->tensor(b1.get_ptr());
+    sem_elem_tensor_t b4 = b3->detensorTranspose();
+    sem_elem_tensor_t b5 = dynamic_cast<SemElemTensor*>(b2->extend(dynamic_cast<SemElem*>(b1.get_ptr())).get_ptr());
+    b1->print(cout << "b1: ") << endl;
+    b2->print(cout << "b2: ") << endl;
+    b3->print(cout << "b3: ") << endl;
+    b4->print(cout << "b4: ") << endl;
+    b5->print(cout << "b5: ") << endl;
+    ASSERT_TRUE(b5->equal(b4));
+  }
+
+  TEST(Nwa_detensor, failing1)
+  {
+    program_bdd_context_t brm = new ProgramBddContext();
+    map<string, int> vars;
+    vars["x1"] = 2;
+    vars["x2"] = 2;
+    vars["x3"] = 2;
+    brm->setIntVars(vars);
+    bdd b = brm->setPost("x2") & brm->setPre("x3") & brm->unsetPost("x3");
+    cout << "b: " << fddset << b << endl;
+    sem_elem_tensor_t b1 = new BinRel(brm.get_ptr(), b);
+    sem_elem_tensor_t b2 = new BinRel(brm.get_ptr(), b);
+    sem_elem_tensor_t b3 = b2->tensor(b1.get_ptr());
+    sem_elem_tensor_t b4 = b3->detensorTranspose();
+    sem_elem_tensor_t b5 = dynamic_cast<SemElemTensor*>(b2->extend(dynamic_cast<SemElem*>(b1.get_ptr())).get_ptr());
+    b1->print(cout << "b1: ") << endl;
+    b2->print(cout << "b2: ") << endl;
+    b3->print(cout << "b3: ") << endl;
+    b4->print(cout << "b4: ") << endl;
+    b5->print(cout << "b5: ") << endl;
+    ASSERT_TRUE(b5->equal(b4));
+  }
+
+  TEST(Nwa_detensor, smallRandom)
+  {
+    program_bdd_context_t brm = new ProgramBddContext();
+    map<string, int> vars;
+    vars["x1"] = 2;
+    vars["x2"] = 2;
+    vars["x3"] = 2;
+    brm->setIntVars(vars);
+    for(unsigned i =0; i < 10; ++i){
+      sem_elem_tensor_t b1 = new BinRel(brm.get_ptr(), brm->tGetRandomTransformer(false, 0));
+      sem_elem_tensor_t b2 = new BinRel(brm.get_ptr(), brm->tGetRandomTransformer(false, 0));
+      sem_elem_tensor_t b3 = b2->tensor(b1.get_ptr());
+      sem_elem_tensor_t b4 = b3->detensorTranspose();
+      sem_elem_tensor_t b5 = dynamic_cast<SemElemTensor*>(b2->extend(dynamic_cast<SemElem*>(b1.get_ptr())).get_ptr());
+      b1->print(cout << "b1: ") << endl;
+      b2->print(cout << "b2: ") << endl;
+      b3->print(cout << "b3: ") << endl;
+      b4->print(cout << "b4: ") << endl;
+      b5->print(cout << "b5: ") << endl;
+      ASSERT_TRUE(b5->equal(b4));
+    }
+  }
 
 }
 
