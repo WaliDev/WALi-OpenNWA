eadef9d65a08d9783ae65af22366c485a895042a
Prathmesh <callpraths@gmail.com>
2013-01-30 16:40:56 -0600

    Added ReadBddAndDetensor to race difference detensor implemenetations

---
+++ Examples/cprover/ReadBddAndDetensor.cpp
@@ -0,0 +1,39 @@
+// ::std
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <ctime>
+// ::wali::util
+#include "wali/util/Timer.hpp"
+#include "wali/domains/binrel/BinRel.hpp"
+#include "wali/domains/binrel/ProgramBddContext.hpp"
+#include "buddy/bdd.h"
+
+using namespace std;
+using namespace wali;
+using namespace wali::domains::binrel;
+
+int main(int argc, char ** argv)
+{
+  if(argc < 2){
+    cerr << "Usage: ./ReadBddAndDetensor bdd_context_file bdd_file" << endl;
+    return 1;
+  }
+
+  ProgramBddContext * con;
+  ifstream baz(argv[1]);
+  con = ProgramBddContext::buildFromHistory(baz);
+  baz.close();
+
+  bdd a;
+  bdd_fnload(argv[2], a);
+
+  binrel_t b = new BinRel(con, a, true);
+
+  wali::util::Timer * t = new wali::util::Timer("Detensor Time");
+  b->detensorTranspose();
+  delete t;
+
+  return 0;
+}
