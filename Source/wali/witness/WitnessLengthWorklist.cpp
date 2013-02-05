1f29f1987d55e5fa5977b3cf39806652f2b51d8a
Rich Joiner <rich@rich-laptop.(none)>
2013-02-05 08:44:48 -0600

    Implemented WitnessLengthWorklist
    Heuristically propagates shorter witnesses during poststar

---
+++ Source/wali/witness/WitnessLengthWorklist.cpp
@@ -0,0 +1,22 @@
+/*!
+ * @author Nicholas Kidd
+ */
+
+#include "wali/Common.hpp"
+#include "wali/witness/WitnessLengthWorklist.hpp"
+#include "wali/wfa/Trans.hpp"
+
+namespace wali
+{
+  namespace witness
+  {
+    WitnessLengthWorklist::WitnessLengthWorklist() : PriorityWorklist<ShorterThan>()
+    {
+    }
+
+    WitnessLengthWorklist::~WitnessLengthWorklist()
+    {
+    }
+  }
+}
+
