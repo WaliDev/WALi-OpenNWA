0d42dabbd962adc159dc2e34575ee9956287d97b
Evan Driscoll <driscoll@cs.wisc.edu>
2013-05-30 10:32:39 -0500

    Test fixtures: move some WFA fixtures to header

--- Tests/unit-tests/Source/wali/wfa/class-wfa/endOfEpsilonChain.cpp
+++ Tests/unit-tests/Source/wali/wfa/class-wfa/endOfEpsilonChain.cpp
@@ -3,6 +3,7 @@
 #include "fixtures/SimpleWeights.hpp"
 #include "fixtures/StringWeight.hpp"
 #include "fixtures/Keys.hpp"
+#include "fixtures.hpp"
 
 using namespace testing;
 using namespace testing::ShortestPathWeights;
@@ -10,86 +11,6 @@ using namespace testing::ShortestPathWeights;
 using wali::wfa::WFA;
 using wali::sem_elem_t;
 using wali::WALI_EPSILON;
-
-//   eps    eps    eps     
-// 1 ---> 2 ---> 3 ---> 4
-struct ChainedEpsilonTransitions
-{
-    WFA wfa;
-    Keys keys;
-
-    ChainedEpsilonTransitions(sem_elem_t first,
-                              sem_elem_t second,
-                              sem_elem_t third)
-    {
-        sem_elem_t zero = first->zero();
-        
-        wfa.addState(keys.st1, zero);
-        wfa.addState(keys.st2, zero);
-        wfa.addState(keys.st3, zero);
-        wfa.addState(keys.st4, zero);
-        wfa.setInitialState(keys.st2);
-
-        wfa.addTrans(keys.st1, WALI_EPSILON, keys.st2, first);
-        wfa.addTrans(keys.st2, WALI_EPSILON, keys.st3, second);
-        wfa.addTrans(keys.st3, WALI_EPSILON, keys.st4, third);
-    }
-};
-
-
-//   eps     a
-// 1 ---> 2 ---> 3
-struct EpsTransThenNormal
-{
-    WFA wfa;
-    Keys keys;
-
-    EpsTransThenNormal(sem_elem_t first,
-                       sem_elem_t second)
-    {
-        sem_elem_t zero = first->zero();
-
-        wfa.addState(keys.st1, zero);
-        wfa.addState(keys.st2, zero);
-        wfa.addState(keys.st3, zero);
-        wfa.setInitialState(keys.st2);
-
-        wfa.addTrans(keys.st1, WALI_EPSILON, keys.st2, first);
-        wfa.addTrans(keys.st2, keys.a, keys.st3, second);
-    }
-};
-
-
-//            eps
-//            ---> 3
-//    eps    /
-//  1 ---> 2
-//           \                          [anti-line-continuation]
-//            ---> 4
-//            eps
-struct Branching
-{
-    WFA wfa;
-    Keys keys;
-
-    Branching(sem_elem_t first,
-              sem_elem_t second,
-              sem_elem_t third)
-    {
-        sem_elem_t zero = first->zero();
-        
-        wfa.addState(keys.st1, zero);
-        wfa.addState(keys.st2, zero);
-        wfa.addState(keys.st3, zero);
-        wfa.addState(keys.st4, zero);
-        wfa.setInitialState(keys.st2);
-
-        wfa.addTrans(keys.st1, WALI_EPSILON, keys.st2, first);
-        wfa.addTrans(keys.st2, WALI_EPSILON, keys.st3, second);
-        wfa.addTrans(keys.st2, WALI_EPSILON, keys.st4, third);
-    }
-};
-
 
 namespace wali {
     namespace wfa {
