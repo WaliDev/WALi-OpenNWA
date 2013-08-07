f1833d36ffa1ada42bc67dfa559d979fc21138d1
Divy <divy@cs.wisc.edu>
2013-08-07 14:29:12 -0500

    Adding dummy test in  opennwa::query

---
+++ Tests/unit-tests/Source/opennwa/namespace-query/test.cpp
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+
+
+using namespace opennwa;
+using namespace std;
+
+
+namespace opennwa {
+	namespace query {
+
+		TEST(opennwa$query$$test, testAssertTrue)
+		{   
+			
+			EXPECT_TRUE(true);
+
+		}
+
+		TEST(opennwa$query$$test, testAssertFalse)
+		{   
+			
+			EXPECT_FALSE(true);
+
+		}
+
+
+	} // end namespace query
+
+} // end namespace opennwa
