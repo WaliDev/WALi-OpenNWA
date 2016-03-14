6c8c3fe70a4a6d246de6d7797d188d9264e3e627
Jason Breck <jbreck@cs.wisc.edu>
2016-03-14 15:21:06 -0500

    Added NEWTON_DUET_ROOT and WALI_ROOT environment vars to duet_build.sh

--- duet_build.sh
+++ duet_build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-scons addons || exit 1
-
 TSLRE_LIB_PATH=Examples/cprover/libtslre/build
 TSLRE_INCLUDE_PATH=Examples/cprover/libtslre/redist-tsl-sandbox
+if [ "x$WALI_ROOT" == "x" ]; then
+    #WALI_ROOT=/home/turetsky/WALi-OpenNWA
+    WALI_ROOT=.
+fi
+if [ "x$NEWTON_DUET_ROOT" == "x" ]; then
+    NEWTON_DUET_ROOT=/home/turetsky/duet
+    if [ ! -d "$NEWTON_DUET_ROOT" ]; then
+        echo "Please set NEWTON_DUET_ROOT to the base directory of Duet."
+        echo "  (We must link against NEWTON_DUET_ROOT/_build/duet/src/libduet.so)"
+        exit 1
+    fi
+fi
+
+scons addons || exit 1
 
 g++ -o newton_interface.os -c -Wall -g -O0 -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=1 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D__TSL_REGEXP=1 -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IAddOns/Domains/ThirdParty/include -IAddOns/Domains/Source -IThirdParty/include -I"`ocamlc -where`" Examples/cprover/NewtonOcamlInterface.cpp || exit 1
 
@@ -13,5 +25,5 @@ g++ -o _build64/Examples/cprover/NewtonCompare.o -c -O0 -Wall -g -Wextra -Wforma
 #/usr/bin/g++ -o _build64/Examples/cprover/CallOcaml.o -c -O0 -Wall -g -Wextra -Wformat=2 -Winit-self -Wunused -Wfloat-equal -Wpointer-arith -Wcast-align -Wwrite-strings -Wconversion -Woverloaded-virtual -fdiagnostics-show-option -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include -I"`ocamlc -where`" Examples/cprover/CallOcaml.cpp
 
 echo "***************** Link phase: *****************"
-g++ -g -o _build64/Examples/cprover/NewtonOcaml -Wl,-rpath=/unsup/boost-1.55.0/amd64_rhel6/lib -Wl,-rpath=/home/turetsky/duet/_build/duet/src -Wl,-rpath=/home/turetsky/WALi-OpenNWA -Wl,-rpath=/home/turetsky/WALi-OpenNWA/lib64 -Wl,--start-group _build64/Examples/cprover/NewtonCompare.o -L/unsup/boost-1.55.0/amd64_rhel6/lib -Llib64 -L"`ocamlc -where`" -L/home/turetsky/duet/_build/duet/src -L/home/turetsky/WALi-OpenNWA -L$TSLRE_LIB_PATH -ltslre -lboost_filesystem -lboost_system -lboost_serialization -lrt -lduet -locamlinterface -lwali -lbpparser -lwalidomains -lglog -lbdd -Wl,--end-group || exit 1
+g++ -g -o _build64/Examples/cprover/NewtonOcaml -Wl,-rpath=/unsup/boost-1.55.0/amd64_rhel6/lib -Wl,-rpath=$NEWTON_DUET_ROOT/_build/duet/src -Wl,-rpath=$WALI_ROOT -Wl,-rpath=$WALI_ROOT/lib64 -Wl,--start-group _build64/Examples/cprover/NewtonCompare.o -L/unsup/boost-1.55.0/amd64_rhel6/lib -Llib64 -L"`ocamlc -where`" -L$NEWTON_DUET_ROOT/_build/duet/src -L$WALI_ROOT -L$TSLRE_LIB_PATH -ltslre -lboost_filesystem -lboost_system -lboost_serialization -lrt -lduet -locamlinterface -lwali -lbpparser -lwalidomains -lglog -lbdd -Wl,--end-group || exit 1
 echo "******* Successful end of duet_build.sh *******"
