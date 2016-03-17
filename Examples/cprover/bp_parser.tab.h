240ca38db8de03a233c0610dc9751a42ffaec88c
Jason Breck <jbreck@cs.wisc.edu>
2016-03-16 19:10:46 -0500

    Adding Examples/cprover/bp_parser.tab.c and .h since I don't know their origin.

---
+++ Examples/cprover/bp_parser.tab.h
@@ -0,0 +1,113 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_BP_PARSER_TAB_H_INCLUDED
+# define YY_YY_BP_PARSER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    IDENTIFIER = 258,
+    TICKED_IDENTIFIER = 259,
+    INTEGER = 260,
+    NONDET = 261,
+    ASSIGN = 262,
+    EQ_OP = 263,
+    NE_OP = 264,
+    IMPLIES = 265,
+    DECL = 266,
+    ENFORCE = 267,
+    AND = 268,
+    OR = 269,
+    TERNARY = 270,
+    IF = 271,
+    THEN = 272,
+    ELSE = 273,
+    ELSIF = 274,
+    FI = 275,
+    WHILE = 276,
+    DO = 277,
+    OD = 278,
+    RETURN = 279,
+    SKIP = 280,
+    GOTO = 281,
+    BEG = 282,
+    END = 283,
+    BOOL = 284,
+    VOID = 285,
+    ASSERT = 286,
+    ASSUME = 287,
+    PRINT = 288,
+    CHOOSE = 289,
+    CONSTRAIN = 290,
+    DEAD = 291,
+    ABORTIF = 292,
+    DFS = 293,
+    START_THREAD = 294,
+    END_THREAD = 295,
+    SYNC = 296,
+    ATOMIC_BEGIN = 297,
+    ATOMIC_END = 298
+  };
+#endif
+
+/* Value type.  */
+
+/* Location type.  */
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+};
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+
+extern YYSTYPE yylval;
+extern YYLTYPE yylloc;
+int yyparse (void);
+
+#endif /* !YY_YY_BP_PARSER_TAB_H_INCLUDED  */
