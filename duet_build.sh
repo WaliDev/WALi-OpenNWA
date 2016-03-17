19fa8e572bc85f9f2bbc31183f6c4c13a187a5cb
Jason Breck <jbreck@cs.wisc.edu>
2016-03-17 12:13:21 -0500

    Minor improvements for new installations.

--- duet_build.sh
+++ duet_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+BOOST_PATH=/unsup/boost-1.55.0/amd64_rhel6
 TSLRE_LIB_PATH=Examples/cprover/libtslre/build
 TSLRE_INCLUDE_PATH=Examples/cprover/libtslre/redist-tsl-sandbox
 if [ "x$WALI_ROOT" == "x" ]; then
@@ -22,14 +23,14 @@ fi
 
 scons addons || exit 1
 
-g++ -o newton_interface.os -c -Wall -g -O0 -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=1 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D__TSL_REGEXP=1 -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IAddOns/Domains/ThirdParty/include -IAddOns/Domains/Source -IThirdParty/include -I"`ocamlc -where`" Examples/cprover/NewtonOcamlInterface.cpp || exit 1
+g++ -o newton_interface.os -c -Wall -g -O0 -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=1 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D__TSL_REGEXP=1 -ISource -I$BOOST_PATH/include -IAddOns/Domains/ThirdParty/include -IAddOns/Domains/Source -IThirdParty/include -I"`ocamlc -where`" Examples/cprover/NewtonOcamlInterface.cpp || exit 1
 
-g++ -o libocamlinterface.so -rdynamic -shared -Wl,-rpath=/unsup/boost-1.55.0/amd64_rhel6/lib -Wl,-rpath=/home/turetsky/OCamlTest newton_interface.os -L/unsup/boost-1.55.0/amd64_rhel6/lib -L/home/turetsky/OCamlTest -lboost_filesystem -lboost_system -lboost_serialization -lrt || exit 1
+g++ -o libocamlinterface.so -rdynamic -shared -Wl,-rpath=$BOOST_PATH/lib -Wl,-rpath=/home/turetsky/OCamlTest newton_interface.os -L$BOOST_PATH/lib -L/home/turetsky/OCamlTest -lboost_filesystem -lboost_system -lboost_serialization -lrt || exit 1
 
-g++ -o _build64/Examples/cprover/NewtonCompare.o -c -O0 -Wall -g -Wextra -Wformat=2 -Winit-self -Wfloat-equal -Wpointer-arith -Wcast-align -Wwrite-strings -Wconversion -Woverloaded-virtual -fdiagnostics-show-option -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -DUSE_DUET=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I$TSLRE_INCLUDE_PATH -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include -I"`ocamlc -where`" Examples/cprover/NewtonCompare.cpp || exit 1
+g++ -o _build64/Examples/cprover/NewtonCompare.o -c -O0 -Wall -g -Wextra -Wformat=2 -Winit-self -Wfloat-equal -Wpointer-arith -Wcast-align -Wwrite-strings -Wconversion -Woverloaded-virtual -fdiagnostics-show-option -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -DUSE_DUET=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I$TSLRE_INCLUDE_PATH -ISource -I$BOOST_PATH/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include -I"`ocamlc -where`" Examples/cprover/NewtonCompare.cpp || exit 1
 
-#/usr/bin/g++ -o _build64/Examples/cprover/CallOcaml.o -c -O0 -Wall -g -Wextra -Wformat=2 -Winit-self -Wunused -Wfloat-equal -Wpointer-arith -Wcast-align -Wwrite-strings -Wconversion -Woverloaded-virtual -fdiagnostics-show-option -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include -I"`ocamlc -where`" Examples/cprover/CallOcaml.cpp
+#/usr/bin/g++ -o _build64/Examples/cprover/CallOcaml.o -c -O0 -Wall -g -Wextra -Wformat=2 -Winit-self -Wunused -Wfloat-equal -Wpointer-arith -Wcast-align -Wwrite-strings -Wconversion -Woverloaded-virtual -fdiagnostics-show-option -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -ISource -I$BOOST_PATH/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include -I"`ocamlc -where`" Examples/cprover/CallOcaml.cpp
 
 echo "***************** Link phase: *****************"
-g++ -g -o _build64/Examples/cprover/NewtonOcaml -Wl,-rpath=/unsup/boost-1.55.0/amd64_rhel6/lib -Wl,-rpath=$NEWTON_DUET_ROOT/_build/duet/src -Wl,-rpath=$WALI_ROOT -Wl,-rpath=$WALI_ROOT/lib64 -Wl,--start-group _build64/Examples/cprover/NewtonCompare.o -L/unsup/boost-1.55.0/amd64_rhel6/lib -Llib64 -L"`ocamlc -where`" -L$NEWTON_DUET_ROOT/_build/duet/src -L$WALI_ROOT -L$TSLRE_LIB_PATH -ltslre -lboost_filesystem -lboost_system -lboost_serialization -lrt -lduet -locamlinterface -lwali -lbpparser -lwalidomains -lglog -lbdd -Wl,--end-group || exit 1
+g++ -g -o _build64/Examples/cprover/NewtonOcaml -Wl,-rpath=$BOOST_PATH/lib -Wl,-rpath=$NEWTON_DUET_ROOT/_build/duet/src -Wl,-rpath=$WALI_ROOT -Wl,-rpath=$WALI_ROOT/lib64 -Wl,--start-group _build64/Examples/cprover/NewtonCompare.o -L$BOOST_PATH/lib -Llib64 -L"`ocamlc -where`" -L$NEWTON_DUET_ROOT/_build/duet/src -L$WALI_ROOT -L$TSLRE_LIB_PATH -ltslre -lboost_filesystem -lboost_system -lboost_serialization -lrt -lduet -locamlinterface -lwali -lbpparser -lwalidomains -lglog -lbdd -Wl,--end-group || exit 1
 echo "******* Successful end of duet_build.sh *******"
--- rebuild_parser.sh
+++ rebuild_parser.sh
@@ -14,6 +14,8 @@
 #                    BplToPds.cpp
 # Its output is:
 #   lib64/libbpparser.so
+BOOST_PATH=/unsup/boost-1.55.0/amd64_rhel6
+
 scons addons
 if [ ! -d "_build64/Examples/cprover" ]; then
     mkdir -p _build64/Examples/cprover
@@ -24,18 +26,18 @@ cp Examples/cprover/bp_scanner.l _build64/Examples/cprover/
 flex -t _build64/Examples/cprover/bp_scanner.l > _build64/Examples/cprover/bp_scanner.c || exit 1
 
 
-/usr/bin/gcc -o _build64/Examples/cprover/bp_scanner.os -c -Wall -g -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I/home/tsl-sandbox -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include _build64/Examples/cprover/bp_scanner.c || exit 1
+/usr/bin/gcc -o _build64/Examples/cprover/bp_scanner.os -c -Wall -g -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I/home/tsl-sandbox -ISource -I$BOOST_PATH/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include _build64/Examples/cprover/bp_scanner.c || exit 1
 
 
-/usr/bin/gcc -o _build64/Examples/cprover/bp_parser.os -c -Wall -g -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I/home/tsl-sandbox -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include _build64/Examples/cprover/bp_parser.c || exit 1
+/usr/bin/gcc -o _build64/Examples/cprover/bp_parser.os -c -Wall -g -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I/home/tsl-sandbox -ISource -I$BOOST_PATH/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include _build64/Examples/cprover/bp_parser.c || exit 1
 
 
-/usr/bin/gcc -o _build64/Examples/cprover/ast.os -c -Wall -g -Wextra -Wformat=2 -Winit-self -Wunused -Wfloat-equal -Wpointer-arith -Wcast-align -Wwrite-strings -Wconversion -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I/home/tsl-sandbox -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include Examples/cprover/ast.c || exit 1
+/usr/bin/gcc -o _build64/Examples/cprover/ast.os -c -Wall -g -Wextra -Wformat=2 -Winit-self -Wunused -Wfloat-equal -Wpointer-arith -Wcast-align -Wwrite-strings -Wconversion -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I/home/tsl-sandbox -ISource -I$BOOST_PATH/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include Examples/cprover/ast.c || exit 1
 
-/usr/bin/g++ -o _build64/Examples/cprover/BplToPds.os -c -O0 -Wall -g -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I/home/tsl-sandbox -ISource -I/unsup/boost-1.55.0/amd64_rhel6/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include Examples/cprover/BplToPds.cpp || exit 1
+/usr/bin/g++ -o _build64/Examples/cprover/BplToPds.os -c -O0 -Wall -g -Wextra -fdiagnostics-show-option -fPIC -DBOOST_NO_DEFAULTED_FUNCTIONS=1 -DCHECKED_LEVEL=2 -DEXPORT_GTR_SYMBOLS=0 -DPRATHMEHS_NWA_DETENSOR=0 -DREGEXP_TEST=1 -DTSL_DETENSOR=1 -D_GLIBCXX_DEBUG=1 -D__TSL_REGEXP=1 -I/home/tsl-sandbox -ISource -I$BOOST_PATH/include -IExamples/cprover -IAddOns/Domains/Source -IAddOns/Domains/ThirdParty/include Examples/cprover/BplToPds.cpp || exit 1
 
 echo "***************** Link phase: *****************"
-/usr/bin/g++ -o _build64/Examples/cprover/libbpparser.so -rdynamic -shared -Wl,-rpath=/unsup/boost-1.55.0/amd64_rhel6/lib -Wl,-rpath=/home/turetsky/OCamlTest -Wl,-rpath=/home/turetsky/WALi-OpenNWA/lib64 _build64/Examples/cprover/bp_parser.os _build64/Examples/cprover/bp_scanner.os _build64/Examples/cprover/BplToPds.os _build64/Examples/cprover/ast.os -L/unsup/boost-1.55.0/amd64_rhel6/lib -L/home/turetsky/OCamlTest -Llib64 -lboost_filesystem -lboost_system -lboost_serialization -lrt -ldl -lm -lwali || exit 1
+/usr/bin/g++ -o _build64/Examples/cprover/libbpparser.so -rdynamic -shared -Wl,-rpath=$BOOST_PATH/lib -Wl,-rpath=/home/turetsky/OCamlTest -Wl,-rpath=/home/turetsky/WALi-OpenNWA/lib64 _build64/Examples/cprover/bp_parser.os _build64/Examples/cprover/bp_scanner.os _build64/Examples/cprover/BplToPds.os _build64/Examples/cprover/ast.os -L$BOOST_PATH/lib -L/home/turetsky/OCamlTest -Llib64 -lboost_filesystem -lboost_system -lboost_serialization -lrt -ldl -lm -lwali || exit 1
 
 cp _build64/Examples/cprover/libbpparser.so lib64/libbpparser.so || exit 1
 
