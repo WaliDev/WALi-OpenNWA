fbbee6c4d3d4d79e44122a21a86c8ca9e0c3197e
Ashkan Forouhi <ashkanfb@gmail.com>
2016-04-19 09:31:56 -0500

    An example of assertion checks for a c4b test

--- Examples/cprover/tests/c4b/gcd.c
+++ Examples/cprover/tests/c4b/gcd.c
@@ -3,18 +3,44 @@
 unsigned int tick = 0;
 
 int gcd(int x, int y) {
+	// used for assertions
+	int inX=x;
+	int inY=y;
+
 	if (x <= 0) return y;
 	if (y <= 0) return x;
 	for (;;) {
 		tick++;
 		if (x>y) x -= y;
 		else if (y>x) y -= x;
-		else return x;
-  }
+		else break;
+	}
+	
+	int bnd = (inX>0?inX:0) + (inY>0?inY:0);
+	assert(tick <= bnd);
+	int simpbnd = inX + inY;
+	assert(tick <= simpbnd);
+	
+	return x;
 }
+
+void assertion (int x, int y) {
+	tick = 0;
+	gcd(x, y);
+	
+	int assertionbnd = (x>0?x:0) + (y>0?y:0);
+	assert(tick <= assertionbnd);
+}
+	
 
 int main() 
 {
-	gcd(20, 30);
+	int x = __VERIFIER_nondet_int();
+	int y = __VERIFIER_nondet_int();
+	int mainbnd = (x>0?x:0) + (y>0?y:0);
+
+	assertion(x, y);
+	
+	assert(tick <= mainbnd);
 	return 0;
 }
