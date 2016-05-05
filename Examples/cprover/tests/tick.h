7cb9948ef4f36ebd477ed2b2efd3eacc0b1fef23
Jason Breck <jbreck@cs.wisc.edu>
2016-05-05 12:09:19 -0500

    Updating tick.h and adding tick_hwm.h

--- Examples/cprover/tests/tick.h
+++ Examples/cprover/tests/tick.h
@@ -1,17 +1,12 @@
-int cost; // current (memory) usage
 
-int hwm;  // high water mark: the largest amount of memory that we've
-          //   needed at any point during this execution 
+int __cost; // current (memory) usage
 
-#define init_tick(k) {cost = (k); hwm = (k);}
+#define init_tick(k) {__cost = (k);}
 // You could add a semicolon here, and __VERIFIER_assume(hwm >= cost)
 
 #define tick(k) { \
-                 __VERIFIER_assume(k <= cost); \
-                 __VERIFIER_assume(hwm >= cost); \
-                 __VERIFIER_assume(cost >= 0); \
-                 __VERIFIER_assume(hwm >= 0); \
-                 cost = cost + (k); \
-                 hwm = (hwm >= cost) ? hwm : cost; \
+                 __VERIFIER_assume((k) <= __cost); \
+                 __VERIFIER_assume(__cost >= 0); \
+                 __cost = __cost + (k); \
                  }
 
