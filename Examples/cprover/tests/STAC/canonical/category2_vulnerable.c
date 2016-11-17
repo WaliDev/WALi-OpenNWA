62dab3b47848ae4f22196e3560f1ea1ff0565ab0
Jason Breck <jbreck@cs.wisc.edu>
2016-11-17 15:50:06 -0600

    Adding a first STAC canonical example.

---
+++ Examples/cprover/tests/STAC/canonical/category2_vulnerable.c
@@ -0,0 +1,40 @@
+#include "tick.h"
+
+void doProcess(int n) {
+    for(int i = 0; i < n; i++) {
+        tick(1000);
+        for(int t = 0; t < n; t++) {
+            tick(1);
+        }
+    }
+    __VERIFIER_print_hull(__cost);
+}
+
+int main(int argc, char ** argv) {
+    init_tick(0);
+    if (argc != 1 || atoi(argv[0]) > 99) { return -1; }
+    doProcess(atoi(argv[0]));
+    return 0;
+}
+
+// Java source:
+/*
+
+//Lower Orders are Disregarded
+public class Category2_vulnerable {
+  private static void doProcess(int n) throws InterruptedException {
+    for (int i = 0; i < n; i++) {
+      Thread.sleep(1000);
+      for(int t = 0; t < n; t++){
+        Thread.sleep(1);
+      }
+    }
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    if(args.length!=1 || Integer.parseInt(args[0]) > 99){ System.exit(-1);}
+    doProcess(Integer.parseInt(args[0]));
+  }
+}
+
+*/
