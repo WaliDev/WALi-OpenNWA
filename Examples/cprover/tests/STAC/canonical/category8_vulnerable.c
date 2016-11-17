2f72431822dcdbfb4a649655ce08aee947f93fd5
John Cyphert <cyphertjohn@gmail.com>
2016-11-17 16:38:48 -0600

    added category8_vulnerable.c

---
+++ Examples/cprover/tests/STAC/canonical/category8_vulnerable.c
@@ -0,0 +1,67 @@
+#include "tick.h"
+
+void f3(int p) {
+    int i;
+    for(i=0; i<p; i++) {
+        tick(1);
+    }
+}
+
+void f2(int m, int p) {
+    int i;
+    for(i=0; i<m; i++) {
+        f3(p);
+    }
+}
+
+void f1(int n, int m, int p) {
+    int i;
+    for(i=0; i<n; i++) {
+        f2(m, p);
+    }
+}
+
+int main(int argc, char ** argv) {
+    init_tick(0);
+    if(argc != 3 || atoi(argv[0])>9 || atoi(argv[1])>9 || atoi(argv[2]>99)) return -1;
+    int n = atoi(argv[0]);
+    int m = atoi(argv[1]);
+    int p = atoi(argv[2]);
+    f1(n, m, p);
+}
+
+/*
+
+//Only Consider a Single Dimension of Input
+public class Category8_vulnerable {
+  private static void f1(int n, int m, int p) {
+    for(int i = 0; i < n; i++) {
+      f2(m, p);
+    }
+  }
+  private static void f2(int m, int p) {
+    for(int i = 0; i < m; i++) {
+      f3(p);
+    }
+  }
+  private static void f3(int p) {
+    for(int i = 0; i < p; i++) {
+      try {
+        Thread.sleep(1);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    if(args.length!=3 || Integer.parseInt(args[0]) > 9
+            || Integer.parseInt(args[1]) > 9
+            || Integer.parseInt(args[2]) > 99){ System.exit(-1);}
+    int n = Integer.parseInt(args[0]);
+    int m = Integer.parseInt(args[1]);
+    int p = Integer.parseInt(args[2]);
+    f1(n, m, p);
+  }
+}
+*/
