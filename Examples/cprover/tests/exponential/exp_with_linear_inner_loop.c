3d3a332a3e07c570d7e07b9e3ca3b1d6acb56220
John Cyphert <cyphertjohn@gmail.com>
2017-03-21 11:10:44 -0500

    created an exponential directory with a few more examples and moved some files from recurrences to exponential

---
+++ Examples/cprover/tests/exponential/exp_with_linear_inner_loop.c
@@ -0,0 +1,24 @@
+//x_(n+1) = 2*x_n + n,  x_0 = 1
+//x' = 2^(n+1) - n - 1
+#include "pow.h"
+
+DECLARE_POW(2);
+
+int x = 1;
+void func(int n) {
+    for(int i = 0; i<n; i++){
+        int k = 0;
+        for (int j = 0; j<i; j++){
+          k++;
+        }  
+        x = 2*x + k;
+    }
+}
+
+int main()
+{
+    int n = __VERIFIER_nondet_int();
+    __VERIFIER_assume(n > 0);
+    func(n);
+    assert(x  == 2 * icra_pow2(n) - n - 1);
+}
